d7d189fc4f851f054b03b0074aec64b147b742f3
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2022-05-31 09:58:41 +0200

    modified job.sh

--- examples/plot_scale/rietholzbach/job.zsh
+++ examples/plot_scale/rietholzbach/job.zsh
@@ -1,4 +1,5 @@
 #!/bin/zsh
+# run the RoGeR modeling experiment at Rietholzbach lysimeter
 # run monte carlo
 cd "$PWD"/svat_monte_carlo
 OMP_NUM_THREADS=1 mpirun -n 4 python svat.py
@@ -7,29 +8,23 @@ cd ..
 cd "$PWD"/svat_sensitivity
 OMP_NUM_THREADS=1 mpirun -n 4 python svat.py
 cd ..
-cd "$PWD"/svat_monte_carlo
-python post_processing.py
+# run monte carlo for transport model
+cd "$PWD"/svat_transport_monte_carlo
+OMP_NUM_THREADS=1 mpirun -n 4 python svat_transport.py
 cd ..
-cd "$PWD"/svat_sensitivity
-python post_processing.py
+# run sobol' sequence for transport model
+cd "$PWD"/svat_transport_sensitivity
+OMP_NUM_THREADS=1 mpirun -n 4 python svat_transport.py
 cd ..
-# # run monte carlo for transport model
-# cd "$PWD"/svat_transport_monte_carlo
-# OMP_NUM_THREADS=1 mpirun -n 4 python svat_transport.py
-# python post_processing.py
-# # run sobol' sequence for transport model
-# cd "$PWD"/svat_transport_sensitivity
-# OMP_NUM_THREADS=1 mpirun -n 4 python svat_transport.py
-# python post_processing.py
-# # run bromide benchmark transport model
-# cd "$PWD"/svat_transport_bromide_benchmark
-# python svat_transport.py
-# python post_processing.py
-# # run reverse sensitivity analysis for transport model
-# cd "$PWD"/svat_transport_sensitivity_reverse
-# OMP_NUM_THREADS=1 mpirun -n 4 python svat_transport.py
-# python post_processing.py
-# # run reverse monte carlo for transport model
-# cd "$PWD"/svat_transport_monte_carlo_reverse
-# OMP_NUM_THREADS=1 mpirun -n 4 python svat_transport.py
-# python post_processing.py
+# run bromide benchmark transport model
+cd "$PWD"/svat_transport_bromide_benchmark
+python svat_transport.py
+cd ..
+# run reverse sensitivity analysis for transport model
+cd "$PWD"/svat_transport_sensitivity_reverse
+OMP_NUM_THREADS=1 mpirun -n 4 python svat_transport.py
+cd ..
+# run reverse monte carlo for transport model
+cd "$PWD"/svat_transport_monte_carlo_reverse
+OMP_NUM_THREADS=1 mpirun -n 4 python svat_transport.py
+exit
