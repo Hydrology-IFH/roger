b98e96cf50da3451c6b332a8cc8a184d07ce11f7
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2022-06-09 13:55:46 +0200

    fix bug in k_stress_transp

--- roger/core/crop.py
+++ roger/core/crop.py
@@ -487,14 +487,17 @@ def calc_k_stress_root_growth(state):
 
     k_stress_root_growth = allocate(state.dimensions, ("x", "y", "crops"))
     mask = (vs.lu_id[:, :, npx.newaxis] == vs.crop_type)
-
+    k_stress_root_growth = update(
+        k_stress_root_growth,
+        at[2:-2, 2:-2, :], 1,
+    )
     k_stress_root_growth = update(
         k_stress_root_growth,
         at[2:-2, 2:-2, :], npx.where(mask[2:-2, 2:-2, :], 1, (vs.theta_rz[2:-2, 2:-2, vs.tau, npx.newaxis] - vs.theta_pwp[2:-2, 2:-2, npx.newaxis]) / (vs.theta_water_stress_crop[2:-2, 2:-2, :] - vs.theta_pwp[2:-2, 2:-2, npx.newaxis])),
     )
     vs.k_stress_root_growth = update(
         vs.k_stress_root_growth,
-        at[2:-2, 2:-2, :], npx.nanmax(k_stress_root_growth, axis=-1)
+        at[2:-2, 2:-2, :], npx.nanmin(k_stress_root_growth, axis=-1)
     )
     vs.k_stress_root_growth = update(
         vs.k_stress_root_growth,
@@ -685,11 +688,15 @@ def update_k_stress_transp(state):
     k_stress_transp_crop = allocate(state.dimensions, ("x", "y", "crops"))
     k_stress_transp_crop = update(
         k_stress_transp_crop,
+        at[2:-2, 2:-2, :], 1
+    )
+    k_stress_transp_crop = update(
+        k_stress_transp_crop,
         at[2:-2, 2:-2, :], npx.where(mask[2:-2, 2:-2, :], vs.k_stress_transp_crop[2:-2, 2:-2, :], 0)
     )
     vs.k_stress_transp = update(
         vs.k_stress_transp,
-        at[2:-2, 2:-2], npx.where(npx.any(vs.crop_type[2:-2, 2:-2, :] == 598, axis=-1), vs.k_stress_transp[2:-2, 2:-2], npx.nanmax(k_stress_transp_crop[2:-2, 2:-2, :], axis=-1))
+        at[2:-2, 2:-2], npx.where(npx.any(vs.crop_type[2:-2, 2:-2, :] == 598, axis=-1), vs.k_stress_transp[2:-2, 2:-2], npx.nanmin(k_stress_transp_crop[2:-2, 2:-2, :], axis=-1))
     )
 
     return KernelOutput(k_stress_transp=vs.k_stress_transp)
