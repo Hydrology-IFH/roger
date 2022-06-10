0fd7dbb8af3fa6951f8630c2ae8fe15cdb18f514
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2022-06-10 18:24:31 +0200

    modified batch_runs

--- examples/plot_scale/rietholzbach/svat_transport_monte_carlo/roger_batch.sh
+++ examples/plot_scale/rietholzbach/svat_transport_monte_carlo/roger_batch.sh
@@ -4,7 +4,7 @@
 #SBATCH --job-name=roger_rietholzbach
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=40
-#SBATCH --mem-per-cpu=4000mb
+#SBATCH --mem-per-cpu=2000mb
 #SBATCH --exclusive
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=robin.schwemmle@hydrology.uni-freiburg.de
