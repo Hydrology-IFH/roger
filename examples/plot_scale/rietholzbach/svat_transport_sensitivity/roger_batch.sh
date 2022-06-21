9832653855c5405da6df9d345d80c07c796fd2e6
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2022-06-21 10:04:55 +0200

    modified roger_batch.sh

--- examples/plot_scale/rietholzbach/svat_transport_sensitivity/roger_batch.sh
+++ examples/plot_scale/rietholzbach/svat_transport_sensitivity/roger_batch.sh
@@ -4,7 +4,7 @@
 #SBATCH --job-name=transport_sa
 #SBATCH --nodes=1
 #SBATCH --ntasks=32
-#SBATCH --mem-per-cpu=5000mb 
+#SBATCH --mem-per-cpu=4000mb 
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=robin.schwemmle@hydrology.uni-freiburg.de
 #SBATCH --export=ALL
