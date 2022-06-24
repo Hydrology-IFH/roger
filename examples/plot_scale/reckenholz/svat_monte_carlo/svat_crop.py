bdd2aa3722fa4e82757e105c8cc69606cc7c2316
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2022-06-24 10:40:30 +0200

    reckenholz: fixed bug in collecting dimensions

--- examples/plot_scale/reckenholz/svat_monte_carlo/svat_crop.py
+++ examples/plot_scale/reckenholz/svat_monte_carlo/svat_crop.py
@@ -772,7 +772,7 @@ for lys_experiment in lys_experiments:
         for dfs in diag_files:
             with h5netcdf.File(dfs, 'r', decode_vlen_strings=False) as df:
                 # set dimensions with a dictionary
-                if not dfs.split('/')[-1].split('.')[1] == 'constant' and 'Time' not in list(dict_dim.keys()):
+                if not dfs.split('/')[-1].split('.')[1] == 'constant':
                     dict_dim = {'x': len(df.variables['x']), 'y': len(df.variables['y']), 'n_crop_types': len(df.variables['n_crop_types']), 'crops': len(df.variables['crops']), 'Time': len(df.variables['Time']}
                     time = onp.array(df.variables.get('Time'))
         for dfs in diag_files:
