a40372cda702a48aef3be34135a87c24f357fbaa
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2022-07-01 13:43:14 +0200

    modified -n in write_batch.py

--- examples/plot_scale/rietholzbach/svat_transport_monte_carlo/oxygen18_pft_mc.sh
+++ examples/plot_scale/rietholzbach/svat_transport_monte_carlo/oxygen18_pft_mc.sh
@@ -9,9 +9,9 @@
 #SBATCH --mail-user=robin.schwemmle@hydrology.uni-freiburg.de
 #SBATCH --export=ALL
 #SBATCH --time=30:00:00
- 
+
 # load module dependencies
 module load lib/hdf5/1.12.1-gnu-11.2-openmpi-4.1
- 
+
 # adapt command to your available scheduler / MPI implementation
 mpirun --bind-to core --map-by core -report-bindings python svat_transport.py -b numpy -d cpu -n 200 1 -ns 10000 -tms time-variant_preferential
--- examples/plot_scale/rietholzbach/svat_transport_sensitivity/write_batch.py
+++ examples/plot_scale/rietholzbach/svat_transport_sensitivity/write_batch.py
@@ -25,7 +25,7 @@ for tm in transport_models:
     lines.append('#SBATCH --partition=single\n')
     lines.append(f'#SBATCH --job-name={script_name}\n')
     lines.append('#SBATCH --nodes=1\n')
-    lines.append('#SBATCH --ntasks=32\n')
+    lines.append('#SBATCH --ntasks-per-node=32\n')
     lines.append('#SBATCH --mem=180000\n')
     lines.append('#SBATCH --mail-type=ALL\n')
     lines.append('#SBATCH --mail-user=robin.schwemmle@hydrology.uni-freiburg.de\n')
