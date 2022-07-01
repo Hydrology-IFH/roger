4d8adb1c769ba7aa7d2f69b59d05211f8f371fc6
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2022-07-01 13:34:02 +0200

    modified -n in write_batch.py

--- examples/plot_scale/rietholzbach/svat_transport_monte_carlo_reverse/oxygen18_adt_mcr.sh
+++ examples/plot_scale/rietholzbach/svat_transport_monte_carlo_reverse/oxygen18_adt_mcr.sh
@@ -1,17 +1,17 @@
 #!/bin/bash -l
 #
-#SBATCH --partition=single
+#SBATCH --partition=multiple
 #SBATCH --job-name=oxygen18_adt_mcr
-#SBATCH --nodes=1
-#SBATCH --ntasks=40
+#SBATCH --nodes=5
+#SBATCH --ntasks-per-node=40
 #SBATCH --mem=180000
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=robin.schwemmle@hydrology.uni-freiburg.de
 #SBATCH --export=ALL
-#SBATCH --time=72:00:00
+#SBATCH --time=30:00:00
  
 # load module dependencies
 module load lib/hdf5/1.12.1-gnu-11.2-openmpi-4.1
  
 # adapt command to your available scheduler / MPI implementation
-mpirun --bind-to core --map-by core -report-bindings python svat_transport.py -b numpy -d cpu -n 40 1 -ns 10000 -tms time-variant_advection-dispersion
+mpirun --bind-to core --map-by core -report-bindings python svat_transport.py -b numpy -d cpu -n 200 1 -ns 10000 -tms time-variant_advection-dispersion
