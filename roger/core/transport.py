a9c024b098fdce28b6a84dfe980e4aaef6d94533
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2022-07-01 17:04:01 +0200

    sanity check of SAS solution works only for numpy backend

--- roger/core/transport.py
+++ roger/core/transport.py
@@ -211,13 +211,13 @@ def calc_tt(state, SA, sa, flux, sas_params):
         at[2:-2, 2:-2, :], npx.where(flux[2:-2, 2:-2, npx.newaxis] <= 0, 0, tt[2:-2, 2:-2, :]),
     )
 
-    # sanity check of SAS function
+    # # sanity check of SAS function (works only for numpy backend)
     # mask = npx.isclose(npx.sum(tt, axis=-1) * flux, flux, atol=1e-02)
     # if not npx.all(mask[2:-2, 2:-2]):
     #     raise RuntimeError(f"Solution of SAS function diverged at iteration {vs.itt}")
-    mask1 = (tt * flux[:, :, npx.newaxis] - sa[:, :, 1, :] > 1e-02)
-    if npx.any(mask1[2:-2, 2:-2, :]):
-        raise RuntimeError(f"Solution of SAS function diverged at iteration {vs.itt}")
+    # mask1 = (tt * flux[:, :, npx.newaxis] - sa[:, :, 1, :] > 1e-02)
+    # if npx.any(mask1[2:-2, 2:-2, :]):
+    #     raise RuntimeError(f"Solution of SAS function diverged at iteration {vs.itt}")
 
     return tt
 
