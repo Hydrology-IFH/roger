0355e1285b42f619ce0f2e446b06472c3bdd8495
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2022-07-12 17:35:06 +0200

    added batch files for gpu computing. replaced NaN with nan.

---
+++ examples/plot_scale/rietholzbach/svat_transport_monte_carlo/oxygen18_pft_mc_gpu.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+#SBATCH --partition=gpu_8
+#SBATCH --job-name=oxygen18_pft_mc
+#SBATCH --gres=gpu:1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=752000
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=robin.schwemmle@hydrology.uni-freiburg.de
+#SBATCH --export=ALL
+#SBATCH --time=8:00:00
+ 
+# load module dependencies
+module load devel/cudnn/9.2
+module load devel/cuda/11.4
+ 
+python svat_transport.py -b jax -d gpu -ns 10000 -tms time-variant_preferential
