a2c9ff75432cf841bd69770ebf96d8090558ed91
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2022-07-12 16:25:05 +0200

    rietholzbach: modified batch

--- examples/plot_scale/rietholzbach/svat_transport_sensitivity/oxygen18_pi_sa.sh
+++ examples/plot_scale/rietholzbach/svat_transport_sensitivity/oxygen18_pi_sa.sh
@@ -8,10 +8,10 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=robin.schwemmle@hydrology.uni-freiburg.de
 #SBATCH --export=ALL
-#SBATCH --time=48:00:00
+#SBATCH --time=24:00:00
  
 # load module dependencies
 module load lib/hdf5/1.12.1-gnu-11.2-openmpi-4.1
  
 # adapt command to your available scheduler / MPI implementation
-mpirun --bind-to core --map-by core -report-bindings python svat_transport.py -b numpy -d cpu -n 32 1 -tms piston
+mpirun --bind-to core --map-by core -report-bindings python svat_transport.py -b jax -d cpu -n 32 1 -tms piston
