4f3755d87ccee97745d4f89b130b8d3e969b4c22
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2022-07-24 22:47:22 +0200

    rietholzbach: modified job scripts

--- examples/plot_scale/rietholzbach/svat_transport_monte_carlo_reverse/oxygen18_pft_mcr_moab.sh
+++ examples/plot_scale/rietholzbach/svat_transport_monte_carlo_reverse/oxygen18_pft_mcr_moab.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#PBS -l nodes=5:ppn=20
+#PBS -l nodes=5:ppn=10
 #PBS -l walltime=30:00:00
-#PBS -l pmem=6000mb
+#PBS -l pmem=12000mb
 #PBS -N oxygen18_pft_mcr
 #PBS -m bea
 #PBS -M robin.schwemmle@hydrology.uni-freiburg.de
@@ -11,4 +11,4 @@ module load lib/hdf5/1.12.0-openmpi-4.1-gnu-9.2
 export OMP_NUM_THREADS=1
  
 # adapt command to your available scheduler / MPI implementation
-mpirun --bind-to core --map-by core -report-bindings python svat_transport.py -b jax -d cpu -n 100 1 -tms time-variant_preferential
+mpirun --bind-to core --map-by core -report-bindings python svat_transport.py -b jax -d cpu -n 50 1 -tms time-variant_preferential
