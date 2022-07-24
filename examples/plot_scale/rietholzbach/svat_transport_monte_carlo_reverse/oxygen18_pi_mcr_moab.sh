7e4534d42a9924eea2a8999883cb33b6dbfb6c77
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2022-07-24 23:37:50 +0200

    rietholzbach: modified job scripts

--- examples/plot_scale/rietholzbach/svat_transport_monte_carlo_reverse/oxygen18_pi_mcr_moab.sh
+++ examples/plot_scale/rietholzbach/svat_transport_monte_carlo_reverse/oxygen18_pi_mcr_moab.sh
@@ -11,6 +11,7 @@ module load lib/hdf5/1.12.0-openmpi-4.1-gnu-9.2
 export OMP_NUM_THREADS=1
 eval "$(conda shell.bash hook)"
 conda activate roger-mpi
+cd /home/fr/fr_fr/fr_rs1092/roger/examples/plot_scale/rietholzbach/svat_transport_monte_carlo_reverse
  
 # adapt command to your available scheduler / MPI implementation
 mpirun --bind-to core --map-by core -report-bindings python svat_transport.py -b jax -d cpu -n 50 1 -tms piston
--- examples/plot_scale/rietholzbach/svat_transport_sensitivity/oxygen18_pft_sa_moab.sh
+++ examples/plot_scale/rietholzbach/svat_transport_sensitivity/oxygen18_pft_sa_moab.sh
@@ -11,6 +11,7 @@ module load lib/hdf5/1.12.0-openmpi-4.1-gnu-9.2
 export OMP_NUM_THREADS=1
 eval "$(conda shell.bash hook)"
 conda activate roger-mpi
+cd /home/fr/fr_fr/fr_rs1092/roger/examples/plot_scale/rietholzbach/svat_transport_sensitivity
  
 # adapt command to your available scheduler / MPI implementation
 mpirun --bind-to core --map-by core -report-bindings python svat_transport.py -b jax -d cpu -n 32 1 -tms time-variant_preferential
