ae441f220d381c6af436cd83ae315a8e84fbefab
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2022-08-23 16:56:57 +0200

    modified job scripts for post-processing

--- examples/plot_scale/rietholzbach/post_processing.py
+++ examples/plot_scale/rietholzbach/post_processing.py
@@ -13,117 +13,130 @@ sns.set_style('ticks', {'xtick.major.size': 8, 'ytick.major.size': 8})
 sns.set_context("paper", font_scale=1.5)
 import roger.tools.evaluation as eval_utils
 import roger.tools.labels as labs
+import click
 
-base_path = Path(__file__).parent
-# directory of figures
-base_path_figs = base_path / "figures"
-if not os.path.exists(base_path_figs):
-    os.mkdir(base_path_figs)
 
-# load observations (measured data)
-path_obs = base_path / "observations" / "rietholzbach_lysimeter.nc"
-ds_obs = xr.open_dataset(path_obs, engine="h5netcdf")
-# assign date
-days_obs = (ds_obs['Time'].values / onp.timedelta64(24 * 60 * 60, "s"))
-date_obs = num2date(days_obs, units=f"days since {ds_obs['Time'].attrs['time_origin']}", calendar='standard', only_use_cftime_datetimes=False)
-ds_obs = ds_obs.assign_coords(date=("Time", date_obs))
-df_obs = pd.DataFrame(index=date_obs)
-df_obs.loc[:, 'd18O_prec'] = ds_obs['d18O_PREC'].isel(x=0, y=0).values
-df_obs.loc[:, 'd18O_perc'] = ds_obs['d18O_PERC'].isel(x=0, y=0).values
+@click.option("-td", "--tmp-dir", type=str, default=None)
+@click.command("main")
+def main(tmp_dir):
+    if tmp_dir:
+        base_path = Path(tmp_dir)
+    else:
+        base_path = Path(__file__).parent
+    # directory of figures
+    base_path_figs = base_path / "figures"
+    if not os.path.exists(base_path_figs):
+        os.mkdir(base_path_figs)
 
-# measured oxygen-18 in precipitation and percolation
-d18O_prec_mean = onp.round(onp.nanmean(df_obs.loc[:, 'd18O_prec'].values), 2)
-d18O_perc_mean = onp.round(onp.nanmean(df_obs.loc[:, 'd18O_perc'].values), 2)
-fig, axs = plt.subplots(2, 1, figsize=(10, 6))
-axs[0].plot(df_obs.index,
-            df_obs.loc[:, 'd18O_prec'].fillna(method='bfill'),
-            '-', color='blue')
-axs[0].plot(df_obs.index,
-            df_obs.loc[:, 'd18O_prec'],
-            '.', color='blue')
-axs[0].set_ylabel(r'$\delta^{18}$O [‰]')
-axs[0].set_ylim([-20,0])
-axs[0].set_xlim(df_obs.index[0], df_obs.index[-1])
-axs[1].plot(df_obs.index, df_obs.loc[:, 'd18O_perc'].fillna(method='bfill'),
-         '-', color='grey')
-axs[1].plot(df_obs.index, df_obs.loc[:, 'd18O_perc'],
-         '.', color='grey')
-axs[1].set_ylabel(r'$\delta^{18}$O [‰]')
-axs[1].set_xlabel('Time [year]')
-axs[1].set_ylim([-20,0])
-axs[1].set_xlim(df_obs.index[0], df_obs.index[-1])
-fig.tight_layout()
-fig.text(0.115, 0.92, "(a)", ha="center", va="center")
-fig.text(0.89, 0.615, r"$\overline{\delta^{18}O}_{prec}$: %s" % (d18O_prec_mean), ha="center", va="center")
-fig.text(0.89, 0.155, r"$\overline{\delta^{18}O}_{perc}$: %s" % (d18O_perc_mean), ha="center", va="center")
-fig.text(0.115, 0.46, "(b)", ha="center", va="center")
-file = base_path_figs / 'observed_d18O_prec_perc.pdf'
-fig.savefig(file, dpi=250)
-plt.close(fig=fig)
+    # load observations (measured data)
+    path_obs = base_path / "observations" / "rietholzbach_lysimeter.nc"
+    ds_obs = xr.open_dataset(path_obs, engine="h5netcdf")
+    # assign date
+    days_obs = (ds_obs['Time'].values / onp.timedelta64(24 * 60 * 60, "s"))
+    date_obs = num2date(days_obs, units=f"days since {ds_obs['Time'].attrs['time_origin']}", calendar='standard', only_use_cftime_datetimes=False)
+    ds_obs = ds_obs.assign_coords(date=("Time", date_obs))
+    df_obs = pd.DataFrame(index=date_obs)
+    df_obs.loc[:, 'd18O_prec'] = ds_obs['d18O_PREC'].isel(x=0, y=0).values
+    df_obs.loc[:, 'd18O_perc'] = ds_obs['d18O_PERC'].isel(x=0, y=0).values
 
-# load best monte carlo run
-states_hm_file = base_path / "svat" / "states_hm.nc"
-ds_sim_hm = xr.open_dataset(states_hm_file, engine="h5netcdf")
-# assign date
-days_sim_hm = (ds_sim_hm['Time'].values / onp.timedelta64(24 * 60 * 60, "s"))
-date_sim_hm = num2date(days_sim_hm, units=f"days since {ds_sim_hm['Time'].attrs['time_origin']}", calendar='standard', only_use_cftime_datetimes=False)
-ds_sim_hm = ds_sim_hm.assign_coords(date=("Time", date_sim_hm))
+    # measured oxygen-18 in precipitation and percolation
+    d18O_prec_mean = onp.round(onp.nanmean(df_obs.loc[:, 'd18O_prec'].values), 2)
+    d18O_perc_mean = onp.round(onp.nanmean(df_obs.loc[:, 'd18O_perc'].values), 2)
+    fig, axs = plt.subplots(2, 1, figsize=(10, 6))
+    axs[0].plot(df_obs.index,
+                df_obs.loc[:, 'd18O_prec'].fillna(method='bfill'),
+                '-', color='blue')
+    axs[0].plot(df_obs.index,
+                df_obs.loc[:, 'd18O_prec'],
+                '.', color='blue')
+    axs[0].set_ylabel(r'$\delta^{18}$O [‰]')
+    axs[0].set_ylim([-20,0])
+    axs[0].set_xlim(df_obs.index[0], df_obs.index[-1])
+    axs[1].plot(df_obs.index, df_obs.loc[:, 'd18O_perc'].fillna(method='bfill'),
+             '-', color='grey')
+    axs[1].plot(df_obs.index, df_obs.loc[:, 'd18O_perc'],
+             '.', color='grey')
+    axs[1].set_ylabel(r'$\delta^{18}$O [‰]')
+    axs[1].set_xlabel('Time [year]')
+    axs[1].set_ylim([-20,0])
+    axs[1].set_xlim(df_obs.index[0], df_obs.index[-1])
+    fig.tight_layout()
+    fig.text(0.115, 0.92, "(a)", ha="center", va="center")
+    fig.text(0.89, 0.615, r"$\overline{\delta^{18}O}_{prec}$: %s" % (d18O_prec_mean), ha="center", va="center")
+    fig.text(0.89, 0.155, r"$\overline{\delta^{18}O}_{perc}$: %s" % (d18O_perc_mean), ha="center", va="center")
+    fig.text(0.115, 0.46, "(b)", ha="center", va="center")
+    file = base_path_figs / 'observed_d18O_prec_perc.pdf'
+    fig.savefig(file, dpi=250)
+    plt.close(fig=fig)
 
-# compare simulation and observation
-vars_obs = ['AET', 'PERC', 'dWEIGHT']
-vars_sim = ['aet', 'q_ss', 'dS']
-for var_obs, var_sim in zip(vars_obs, vars_sim):
-    obs_vals = ds_obs[var_obs].isel(x=0, y=0).values
-    df_obs = pd.DataFrame(index=date_obs, columns=['obs'])
-    df_obs.loc[:, 'obs'] = obs_vals
-    sim_vals = ds_sim_hm[var_sim].isel(x=0, y=0).values
-    # join observations on simulations
-    df_eval = eval_utils.join_obs_on_sim(date_sim_hm, sim_vals, df_obs)
-    df_eval = df_eval.iloc[3:, :]
-    # plot observed and simulated time series
-    fig = eval_utils.plot_obs_sim(df_eval, labs._Y_LABS_DAILY[var_sim])
-    file_str = '%s.pdf' % (var_sim)
-    path_fig = base_path_figs / file_str
-    fig.savefig(path_fig, dpi=250)
-    # plot cumulated observed and simulated time series
-    fig = eval_utils.plot_obs_sim_cum(df_eval, labs._Y_LABS_CUM[var_sim], x_lab='Time [year]')
-    file_str = '%s_cum.pdf' % (var_sim)
-    path_fig = base_path_figs / file_str
-    fig.savefig(path_fig, dpi=250)
-    fig = eval_utils.plot_obs_sim_cum_year_facet(df_eval, labs._Y_LABS_CUM[var_sim], x_lab='Time\n[day-month-hydyear]')
-    file_str = '%s_cum_year_facet.pdf' % (var_sim)
-    path_fig = base_path_figs / file_str
-    fig.savefig(path_fig, dpi=250)
+    # load best monte carlo run
+    states_hm_file = base_path / "svat" / "states_hm.nc"
+    ds_sim_hm = xr.open_dataset(states_hm_file, engine="h5netcdf")
+    # assign date
+    days_sim_hm = (ds_sim_hm['Time'].values / onp.timedelta64(24 * 60 * 60, "s"))
+    date_sim_hm = num2date(days_sim_hm, units=f"days since {ds_sim_hm['Time'].attrs['time_origin']}", calendar='standard', only_use_cftime_datetimes=False)
+    ds_sim_hm = ds_sim_hm.assign_coords(date=("Time", date_sim_hm))
 
-vars_obs = ['PREC']
-vars_sim = ['prec']
-for var_obs, var_sim in zip(vars_obs, vars_sim):
-    obs_vals = ds_obs[var_obs].isel(x=0, y=0).values
-    df_obs = pd.DataFrame(index=date_obs, columns=['obs'])
-    df_obs.loc[:, 'obs'] = obs_vals
-    # plot observed time series
-    fig = eval_utils.plot_sim(df_obs, labs._Y_LABS_DAILY[var_sim])
-    file_str = '%s.pdf' % (var_sim)
-    path_fig = base_path_figs / file_str
-    fig.savefig(path_fig, dpi=250)
-    # plot cumulated observed time series
-    fig = eval_utils.plot_sim_cum(df_obs, labs._Y_LABS_CUM[var_sim], x_lab='Time [year]')
-    file_str = '%s_cum.pdf' % (var_sim)
-    path_fig = base_path_figs / file_str
-    fig.savefig(path_fig, dpi=250)
-    fig = eval_utils.plot_sim_cum_year_facet(df_obs, labs._Y_LABS_CUM[var_sim], x_lab='Time\n[day-month-hydyear]')
-    file_str = '%s_cum_year_facet.pdf' % (var_sim)
-    path_fig = base_path_figs / file_str
-    fig.savefig(path_fig, dpi=250)
+    # compare simulation and observation
+    vars_obs = ['AET', 'PERC', 'dWEIGHT']
+    vars_sim = ['aet', 'q_ss', 'dS']
+    for var_obs, var_sim in zip(vars_obs, vars_sim):
+        obs_vals = ds_obs[var_obs].isel(x=0, y=0).values
+        df_obs = pd.DataFrame(index=date_obs, columns=['obs'])
+        df_obs.loc[:, 'obs'] = obs_vals
+        sim_vals = ds_sim_hm[var_sim].isel(x=0, y=0).values
+        # join observations on simulations
+        df_eval = eval_utils.join_obs_on_sim(date_sim_hm, sim_vals, df_obs)
+        df_eval = df_eval.iloc[3:, :]
+        # plot observed and simulated time series
+        fig = eval_utils.plot_obs_sim(df_eval, labs._Y_LABS_DAILY[var_sim])
+        file_str = '%s.pdf' % (var_sim)
+        path_fig = base_path_figs / file_str
+        fig.savefig(path_fig, dpi=250)
+        # plot cumulated observed and simulated time series
+        fig = eval_utils.plot_obs_sim_cum(df_eval, labs._Y_LABS_CUM[var_sim], x_lab='Time [year]')
+        file_str = '%s_cum.pdf' % (var_sim)
+        path_fig = base_path_figs / file_str
+        fig.savefig(path_fig, dpi=250)
+        fig = eval_utils.plot_obs_sim_cum_year_facet(df_eval, labs._Y_LABS_CUM[var_sim], x_lab='Time\n[day-month-hydyear]')
+        file_str = '%s_cum_year_facet.pdf' % (var_sim)
+        path_fig = base_path_figs / file_str
+        fig.savefig(path_fig, dpi=250)
 
-vars_obs = ['TA']
-vars_sim = ['ta']
-for var_obs, var_sim in zip(vars_obs, vars_sim):
-    obs_vals = ds_obs[var_obs].isel(x=0, y=0).values
-    df_obs = pd.DataFrame(index=date_obs, columns=['obs'])
-    df_obs.loc[:, 'obs'] = obs_vals
-    # plot observed time series
-    fig = eval_utils.plot_sim(df_obs, labs._Y_LABS_DAILY[var_sim])
-    file_str = '%s.pdf' % (var_obs)
-    path_fig = base_path_figs / file_str
-    fig.savefig(path_fig, dpi=250)
+    vars_obs = ['PREC']
+    vars_sim = ['prec']
+    for var_obs, var_sim in zip(vars_obs, vars_sim):
+        obs_vals = ds_obs[var_obs].isel(x=0, y=0).values
+        df_obs = pd.DataFrame(index=date_obs, columns=['obs'])
+        df_obs.loc[:, 'obs'] = obs_vals
+        # plot observed time series
+        fig = eval_utils.plot_sim(df_obs, labs._Y_LABS_DAILY[var_sim])
+        file_str = '%s.pdf' % (var_sim)
+        path_fig = base_path_figs / file_str
+        fig.savefig(path_fig, dpi=250)
+        # plot cumulated observed time series
+        fig = eval_utils.plot_sim_cum(df_obs, labs._Y_LABS_CUM[var_sim], x_lab='Time [year]')
+        file_str = '%s_cum.pdf' % (var_sim)
+        path_fig = base_path_figs / file_str
+        fig.savefig(path_fig, dpi=250)
+        fig = eval_utils.plot_sim_cum_year_facet(df_obs, labs._Y_LABS_CUM[var_sim], x_lab='Time\n[day-month-hydyear]')
+        file_str = '%s_cum_year_facet.pdf' % (var_sim)
+        path_fig = base_path_figs / file_str
+        fig.savefig(path_fig, dpi=250)
+
+    vars_obs = ['TA']
+    vars_sim = ['ta']
+    for var_obs, var_sim in zip(vars_obs, vars_sim):
+        obs_vals = ds_obs[var_obs].isel(x=0, y=0).values
+        df_obs = pd.DataFrame(index=date_obs, columns=['obs'])
+        df_obs.loc[:, 'obs'] = obs_vals
+        # plot observed time series
+        fig = eval_utils.plot_sim(df_obs, labs._Y_LABS_DAILY[var_sim])
+        file_str = '%s.pdf' % (var_obs)
+        path_fig = base_path_figs / file_str
+        fig.savefig(path_fig, dpi=250)
+    return
+
+
+if __name__ == "__main__":
+    main()
