30e060d3874c4726b1d664f1a41581e1846d9282
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2022-08-24 10:04:12 +0200

    reckenholz: modified submit_moab

--- examples/plot_scale/reckenholz/svat_monte_carlo/submit_moab.sh
+++ examples/plot_scale/reckenholz/svat_monte_carlo/submit_moab.sh
@@ -4,5 +4,5 @@ cd ~/roger/examples/plot_scale/reckenholz/svat_monte_carlo
 FILES="$PWD/*_mc_moab.sh"
 for f in $FILES
 do
-  qsub -q short ./$f
+  qsub -q short $f
 done
--- examples/plot_scale/reckenholz/svat_sensitivity/submit_moab.sh
+++ examples/plot_scale/reckenholz/svat_sensitivity/submit_moab.sh
@@ -4,5 +4,5 @@ cd ~/roger/examples/plot_scale/reckenholz/svat_sensitivity
 FILES="$PWD/*_sa_moab.sh"
 for f in $FILES
 do
-  qsub -q short ./$f
+  qsub -q short $f
 done
