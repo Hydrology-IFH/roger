8a3743ec67c24c371b5a359a9da3f82c0d3fba0c
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2022-08-25 11:42:56 +0200

    rietholzbach: modified job scripts

--- examples/plot_scale/rietholzbach/svat_sensitivity/serial_moab.sh
+++ examples/plot_scale/rietholzbach/svat_sensitivity/serial_moab.sh
@@ -14,7 +14,7 @@ conda activate roger
 cd /home/fr/fr_fr/fr_rs1092/roger/examples/plot_scale/rietholzbach/svat_sensitivity
 
 # adapt command to your available scheduler / MPI implementation
-python svat.py -td "${TMPDIR}"
+python svat.py -b numpy -d cpu -td "${TMPDIR}"
 
 # Write output to temporary SSD of computing node
 echo "Write output to $TMPDIR"
