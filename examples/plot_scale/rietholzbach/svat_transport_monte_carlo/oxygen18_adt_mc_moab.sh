99cc0f19dd6ef0a72fad59cb3f3d971d6b48c79e
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2022-08-27 18:30:10 +0200

    rietholzbach: modified job scripts

--- examples/plot_scale/rietholzbach/svat_transport_monte_carlo/oxygen18_adt_mc_moab.sh
+++ examples/plot_scale/rietholzbach/svat_transport_monte_carlo/oxygen18_adt_mc_moab.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#PBS -l nodes=5:ppn=20:gpus=4:default
+#PBS -l nodes=8:ppn=25
 #PBS -l walltime=48:00:00
 #PBS -l pmem=2000mb
 #PBS -N oxygen18_adt_mc
@@ -7,14 +7,12 @@
 #PBS -M robin.schwemmle@hydrology.uni-freiburg.de
  
 # load module dependencies
-module load mpi/openmpi/4.1-gnu-9.2-cuda-11.4
 module load lib/hdf5/1.12.0-openmpi-4.1-gnu-9.2
-module load lib/cudnn/8.2-cuda-11.4
 export OMP_NUM_THREADS=1
 eval "$(conda shell.bash hook)"
-conda activate roger-gpu
+conda activate roger-mpi
 cd /home/fr/fr_fr/fr_rs1092/roger/examples/plot_scale/rietholzbach/svat_transport_monte_carlo
  
 # adapt command to your available scheduler / MPI implementation
-mpirun --bind-to core --map-by core -report-bindings python svat_transport.py -b jax -d gpu -n 100 1 -tms time-variant_advection-dispersion -td /beegfs/work/workspace/ws/fr_rs1092-workspace-0/rietholzbach/svat_transport_monte_carlo
+mpirun --bind-to core --map-by core -report-bindings python svat_transport.py -b numpy -d cpu -n 200 1 -tms time-variant_advection-dispersion -td /beegfs/work/workspace/ws/fr_rs1092-workspace-0/rietholzbach/svat_transport_monte_carlo
 mkdir -p /beegfs/work/workspace/ws/fr_rs1092-workspace-0/rietholzbach/svat_transport_monte_carlo
