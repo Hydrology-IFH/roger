929c4ee7f63e6a7f9ad5459434a68077331550d2
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2022-08-27 17:21:10 +0200

    rietholzbach: modified job scripts

--- examples/plot_scale/rietholzbach/svat_transport_sensitivity/oxygen18_pf_sa_moab.sh
+++ examples/plot_scale/rietholzbach/svat_transport_sensitivity/oxygen18_pf_sa_moab.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #PBS -l nodes=2:ppn=16
-#PBS -l walltime=30:00:00
+#PBS -l walltime=48:00:00
 #PBS -l pmem=8000mb
 #PBS -N oxygen18_pf_sa
 #PBS -m bea
@@ -14,10 +14,4 @@ conda activate roger-mpi
 cd /home/fr/fr_fr/fr_rs1092/roger/examples/plot_scale/rietholzbach/svat_transport_sensitivity
  
 # adapt command to your available scheduler / MPI implementation
-mpirun --bind-to core --map-by socket:PE=5 -report-bindings python svat_transport.py -b jax -d cpu -n 32 1 -tms preferential -td "${TMPDIR}"
-# Write output to temporary SSD of computing node
-echo "Write output to $TMPDIR"
-# Move output from temporary SSD to workspace
-echo "Move output to /beegfs/work/workspace/ws/fr_rs1092-workspace-0/rietholzbach/svat_transport_sensitivity"
-mkdir -p /beegfs/work/workspace/ws/fr_rs1092-workspace-0/rietholzbach/svat_transport_sensitivity
-mv "${TMPDIR}"/*.nc /beegfs/work/workspace/ws/fr_rs1092-workspace-0/rietholzbach/svat_transport_sensitivity
+mpirun --bind-to core --map-by core -report-bindings python svat_transport.py -b jax -d cpu -n 32 1 -tms preferential -td /beegfs/work/workspace/ws/fr_rs1092-workspace-0/rietholzbach/svat_transport_sensitivity
