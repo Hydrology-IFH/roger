6568b524014bf8ddc1fd3fee52ecc022bbd2c705
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2022-08-28 14:43:06 +0200

    rietholzbach: modified job scripts

--- examples/plot_scale/rietholzbach/svat_transport_monte_carlo/oxygen18_ad_mc_moab.sh
+++ examples/plot_scale/rietholzbach/svat_transport_monte_carlo/oxygen18_ad_mc_moab.sh
@@ -5,7 +5,7 @@
 #PBS -N oxygen18_ad_mc
 #PBS -m bea
 #PBS -M robin.schwemmle@hydrology.uni-freiburg.de
- 
+
 # load module dependencies
 module load lib/hdf5/1.12.0-openmpi-4.1-gnu-9.2
 module unload devel/cuda/10.1
@@ -13,9 +13,9 @@ export OMP_NUM_THREADS=1
 eval "$(conda shell.bash hook)"
 conda activate roger-mpi
 cd /home/fr/fr_fr/fr_rs1092/roger/examples/plot_scale/rietholzbach/svat_transport_monte_carlo
- 
+
 # adapt command to your available scheduler / MPI implementation
-mpirun --bind-to core --map-by core -report-bindings python svat_transport.py -b numpy -d cpu -n 25 1 -tms advection-dispersion -td "${TMPDIR}"
+mpirun --bind-to core --map-by core -report-bindings python svat_transport.py -b jax -d cpu -n 25 1 -tms advection-dispersion -td "${TMPDIR}"
 mkdir -p /beegfs/work/workspace/ws/fr_rs1092-workspace-0/rietholzbach/svat_transport_monte_carlo
 # Write output to temporary SSD of computing node
 echo "Write output to $TMPDIR"
