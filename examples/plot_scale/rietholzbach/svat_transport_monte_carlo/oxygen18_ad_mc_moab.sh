31e73b35c20ec1900066c5780b246d787971e9a5
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2022-08-28 15:06:53 +0200

    rietholzbach: modified job scripts

--- examples/plot_scale/rietholzbach/svat_transport_monte_carlo/oxygen18_ad_mc_moab.sh
+++ examples/plot_scale/rietholzbach/svat_transport_monte_carlo/oxygen18_ad_mc_moab.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#PBS -l nodes=1:ppn=20
+#PBS -l nodes=2:ppn=20
 #PBS -l walltime=48:00:00
-#PBS -l pmem=6000mb
+#PBS -l pmem=4000mb
 #PBS -N oxygen18_ad_mc
 #PBS -m bea
 #PBS -M robin.schwemmle@hydrology.uni-freiburg.de
@@ -14,11 +14,13 @@ eval "$(conda shell.bash hook)"
 conda activate roger-mpi
 cd /home/fr/fr_fr/fr_rs1092/roger/examples/plot_scale/rietholzbach/svat_transport_monte_carlo
 
-# adapt command to your available scheduler / MPI implementation
-mpirun --bind-to core --map-by core -report-bindings python svat_transport.py -b numpy -d cpu -ns 10000 -n 20 1 -tms advection-dispersion -td "${TMPDIR}"
-mkdir -p /beegfs/work/workspace/ws/fr_rs1092-workspace-0/rietholzbach/svat_transport_monte_carlo
-# Write output to temporary SSD of computing node
-echo "Write output to $TMPDIR"
-# Move output from temporary SSD to workspace
-echo "Move output to /beegfs/work/workspace/ws/fr_rs1092-workspace-0/rietholzbach/svat_transport_monte_carlo"
-mv "${TMPDIR}"/*.nc /beegfs/work/workspace/ws/fr_rs1092-workspace-0/rietholzbach/svat_transport_monte_carlo
+# # adapt command to your available scheduler / MPI implementation
+# mpirun --bind-to core --map-by core -report-bindings python svat_transport.py -b numpy -d cpu -ns 10000 -n 40 1 -tms advection-dispersion -td "${TMPDIR}"
+# mkdir -p /beegfs/work/workspace/ws/fr_rs1092-workspace-0/rietholzbach/svat_transport_monte_carlo
+# # Write output to temporary SSD of computing node
+# echo "Write output to $TMPDIR"
+# # Move output from temporary SSD to workspace
+# echo "Move output to /beegfs/work/workspace/ws/fr_rs1092-workspace-0/rietholzbach/svat_transport_monte_carlo"
+# mv "${TMPDIR}"/*.nc /beegfs/work/workspace/ws/fr_rs1092-workspace-0/rietholzbach/svat_transport_monte_carlo
+
+mpirun --bind-to core --map-by core -report-bindings python svat_transport.py -b numpy -d cpu -ns 10000 -n 40 1 -tms advection-dispersion -td /beegfs/work/workspace/ws/fr_rs1092-workspace-0/rietholzbach/svat_transport_monte_carlo
