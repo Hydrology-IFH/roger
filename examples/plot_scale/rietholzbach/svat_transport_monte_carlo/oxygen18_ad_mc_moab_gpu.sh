97e7db65d9dac6d915e5f2169c37298a2ec9f04a
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2022-08-28 15:48:02 +0200

    rietholzbach: modified job scripts

--- examples/plot_scale/rietholzbach/svat_transport_monte_carlo/oxygen18_ad_mc_moab_gpu.sh
+++ examples/plot_scale/rietholzbach/svat_transport_monte_carlo/oxygen18_ad_mc_moab_gpu.sh
@@ -5,7 +5,7 @@
 #PBS -N oxygen18_ad_mc
 #PBS -m bea
 #PBS -M robin.schwemmle@hydrology.uni-freiburg.de
- 
+
 # load module dependencies
 module load mpi/openmpi/4.1-gnu-9.2-cuda-11.4
 module load lib/hdf5/1.12.0-openmpi-4.1-gnu-9.2
@@ -14,7 +14,7 @@ export OMP_NUM_THREADS=1
 eval "$(conda shell.bash hook)"
 conda activate roger-gpu
 cd /home/fr/fr_fr/fr_rs1092/roger/examples/plot_scale/rietholzbach/svat_transport_monte_carlo
- 
+
 # adapt command to your available scheduler / MPI implementation
-mpirun --bind-to core --map-by core -report-bindings python svat_transport.py -b jax -d gpu -ns 40 -n 4 1 -tms advection-dispersion -td /beegfs/work/workspace/ws/fr_rs1092-workspace-0/rietholzbach/svat_transport_monte_carlo
+mpirun --bind-to core --map-by core -report-bindings python svat_transport.py -b jax -d gpu -ns 400 -n 4 1 -tms advection-dispersion -td /beegfs/work/workspace/ws/fr_rs1092-workspace-0/rietholzbach/svat_transport_monte_carlo
 mkdir -p /beegfs/work/workspace/ws/fr_rs1092-workspace-0/rietholzbach/svat_transport_monte_carlo
