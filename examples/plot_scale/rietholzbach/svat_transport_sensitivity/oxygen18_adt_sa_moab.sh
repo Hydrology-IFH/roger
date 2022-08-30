364c22e951d24be178db5af8f07aff1d21e294eb
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2022-08-30 19:39:55 +0200

    modified logging

--- examples/plot_scale/rietholzbach/svat_transport_sensitivity/oxygen18_adt_sa_moab.sh
+++ examples/plot_scale/rietholzbach/svat_transport_sensitivity/oxygen18_adt_sa_moab.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -l nodes=1:ppn=1
 #PBS -l walltime=48:00:00
-#PBS -l pmem=12000mb
+#PBS -l pmem=8000mb
 #PBS -N oxygen18_adt_sa
 #PBS -m bea
 #PBS -M robin.schwemmle@hydrology.uni-freiburg.de
@@ -14,4 +14,4 @@ conda activate roger-mpi
 cd /home/fr/fr_fr/fr_rs1092/roger/examples/plot_scale/rietholzbach/svat_transport_sensitivity
 
 # adapt command to your available scheduler / MPI implementation
-python svat_transport.py -b numpy -d cpu -ns 32 -tms time-variant_advection-dispersion -td /beegfs/work/workspace/ws/fr_rs1092-workspace-0/rietholzbach/svat_transport_sensitivity
+python svat_transport.py -b numpy -d cpu -ns 128 -tms time-variant_advection-dispersion -td /beegfs/work/workspace/ws/fr_rs1092-workspace-0/rietholzbach/svat_transport_sensitivity
