1ff84e037d7fea61a98a382585be112618c3fc8d
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2022-08-31 09:06:58 +0200

    modified logging

--- examples/plot_scale/rietholzbach/svat_transport_monte_carlo/oxygen18_adt_mc_moab.sh
+++ examples/plot_scale/rietholzbach/svat_transport_monte_carlo/oxygen18_adt_mc_moab.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#PBS -l nodes=5:ppn=20
+#PBS -l nodes=1:ppn=1
 #PBS -l walltime=48:00:00
-#PBS -l pmem=2000mb
+#PBS -l pmem=4000mb
 #PBS -N oxygen18_adt_mc
 #PBS -m bea
 #PBS -M robin.schwemmle@hydrology.uni-freiburg.de
@@ -14,4 +14,4 @@ conda activate roger-mpi
 cd /home/fr/fr_fr/fr_rs1092/roger/examples/plot_scale/rietholzbach/svat_transport_monte_carlo
 
 # adapt command to your available scheduler / MPI implementation
-mpirun --bind-to core --map-by core -report-bindings python svat_transport.py -b jax -d cpu -n 100 1 -tms time-variant_advection-dispersion -td /beegfs/work/workspace/ws/fr_rs1092-workspace-0/rietholzbach/svat_transport_monte_carlo
+python svat_transport.py -b numpy -d cpu -ns 250 -tms time-variant_advection-dispersion -td /beegfs/work/workspace/ws/fr_rs1092-workspace-0/rietholzbach/svat_transport_monte_carlo
--- examples/plot_scale/rietholzbach/svat_transport_monte_carlo/oxygen18_pf_mc_moab.sh
+++ examples/plot_scale/rietholzbach/svat_transport_monte_carlo/oxygen18_pf_mc_moab.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-#PBS -l nodes=5:ppn=20
+#PBS -l nodes=1:ppn=1
 #PBS -l walltime=48:00:00
-#PBS -l pmem=2000mb
+#PBS -l pmem=4000mb
 #PBS -N oxygen18_pf_mc
 #PBS -m bea
 #PBS -M robin.schwemmle@hydrology.uni-freiburg.de
- 
+
 # load module dependencies
 module load lib/hdf5/1.12.0-openmpi-4.1-gnu-9.2
 export OMP_NUM_THREADS=1
 eval "$(conda shell.bash hook)"
 conda activate roger-mpi
 cd /home/fr/fr_fr/fr_rs1092/roger/examples/plot_scale/rietholzbach/svat_transport_monte_carlo
- 
+
 # adapt command to your available scheduler / MPI implementation
-mpirun --bind-to core --map-by core -report-bindings python svat_transport.py -b numpy -d cpu -n 100 1 -tms preferential -td /beegfs/work/workspace/ws/fr_rs1092-workspace-0/rietholzbach/svat_transport_monte_carlo
+python svat_transport.py -b numpy -d cpu -ns 250 -tms preferential -td /beegfs/work/workspace/ws/fr_rs1092-workspace-0/rietholzbach/svat_transport_monte_carlo
