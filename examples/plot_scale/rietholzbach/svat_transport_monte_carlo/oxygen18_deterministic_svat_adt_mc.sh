7defbc475be3d82363bd0c200789050572f764ff
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2022-10-14 12:45:14 +0200

    modified job script

--- examples/plot_scale/rietholzbach/svat_transport_monte_carlo/oxygen18_deterministic_svat_adt_mc.sh
+++ examples/plot_scale/rietholzbach/svat_transport_monte_carlo/oxygen18_deterministic_svat_adt_mc.sh
@@ -5,13 +5,13 @@
 #PBS -N oxygen18_deterministic_svat_adt_mc
 #PBS -m bea
 #PBS -M robin.schwemmle@hydrology.uni-freiburg.de
- 
+
 # load module dependencies
 module load lib/hdf5/1.12.0-openmpi-4.1-gnu-9.2
 export OMP_NUM_THREADS=1
 eval "$(conda shell.bash hook)"
 conda activate roger-mpi
 cd /home/fr/fr_fr/fr_rs1092/roger/examples/plot_scale/rietholzbach/svat_transport_monte_carlo
- 
+
 # adapt command to your available scheduler / MPI implementation
-mpirun --bind-to core --map-by core -report-bindings python svat_transport.py --log-all-processes -b jax -d cpu -n 100 1 -ns 10000 -tms time-variant_advection-dispersion -td /beegfs/work/workspace/ws/fr_rs1092-workspace-0/rietholzbach/svat_transport_monte_carlo -ss deterministic
+mpirun --bind-to core --map-by core -report-bindings python svat_transport.py --log-all-processes -b numpy -d cpu -n 100 1 -ns 10000 -tms time-variant_advection-dispersion -td /beegfs/work/workspace/ws/fr_rs1092-workspace-0/rietholzbach/svat_transport_monte_carlo -ss deterministic
