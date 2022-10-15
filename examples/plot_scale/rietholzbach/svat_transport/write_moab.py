a1083a940cf609d96c6e306e7e9ba524e0f9097f
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2022-10-15 18:00:04 +0200

    modified job scripts

--- examples/plot_scale/rietholzbach/svat_transport/write_moab.py
+++ examples/plot_scale/rietholzbach/svat_transport/write_moab.py
@@ -3,7 +3,7 @@ import subprocess
 import click
 
 
-@click.option("--job-type", type=click.Choice(['serial']), default='serial')
+@click.option("--job-type", type=click.Choice(['serial', 'serial-gpu']), default='serial')
 @click.option("--sas-solver", type=click.Choice(['RK4', 'Euler', 'deterministic']), default='deterministic')
 @click.command("main")
 def main(job_type, sas_solver):
@@ -33,7 +33,7 @@ def main(job_type, sas_solver):
             lines.append('#!/bin/bash\n')
             lines.append('#PBS -l nodes=1:ppn=1\n')
             lines.append('#PBS -l walltime=96:00:00\n')
-            lines.append('#PBS -l pmem=16000mb\n')
+            lines.append('#PBS -l pmem=4000mb\n')
             lines.append(f'#PBS -N {script_name}\n')
             lines.append('#PBS -m bea\n')
             lines.append('#PBS -M robin.schwemmle@hydrology.uni-freiburg.de\n')
@@ -43,6 +43,40 @@ def main(job_type, sas_solver):
             lines.append(f'cd {base_path_binac}\n')
             lines.append(' \n')
             lines.append('python svat_transport.py -b jax -d cpu -tms %s -td "${TMPDIR}" -ss %s\n' % (tms, sas_solver))
+            lines.append('# Move output from local SSD to global workspace\n')
+            lines.append(f'echo "Move output to {output_path_ws.as_posix()}"\n')
+            lines.append('mkdir -p %s\n' % (output_path_ws.as_posix()))
+            lines.append('mv "${TMPDIR}"/*.nc %s\n' % (output_path_ws.as_posix()))
+            file_path = base_path / f'{script_name}.sh'
+            file = open(file_path, "w")
+            file.writelines(lines)
+            file.close()
+            subprocess.Popen(f"chmod +x {script_name}.sh", shell=True)
+
+        elif job_type == 'serial-gpu':
+            tm1 = transport_models_abrev[tm]
+            tms = tm.replace(" ", "_")
+            script_name = f'{tracer}_{sas_solver}_svat_{tm1}_gpu'
+            output_path_ws = base_path_ws / 'rietholzbach' / 'svat_transport'
+            tms = tm.replace(" ", "_")
+            lines = []
+            lines.append('#!/bin/bash\n')
+            lines.append('#PBS -l nodes=1:ppn=1:gpus=1:default\n')
+            lines.append('#PBS -l walltime=24:00:00\n')
+            lines.append('#PBS -l pmem=4000mb\n')
+            lines.append(f'#PBS -N {script_name}\n')
+            lines.append('#PBS -m bea\n')
+            lines.append('#PBS -M robin.schwemmle@hydrology.uni-freiburg.de\n')
+            lines.append(' \n')
+            lines.append('eval "$(conda shell.bash hook)"\n')
+            lines.append('conda activate roger-gpu\n')
+            lines.append(f'cd {base_path_binac}\n')
+            lines.append(' \n')
+            lines.append('# load module dependencies\n')
+            lines.append('module load mpi/openmpi/4.1-gnu-9.2-cuda-11.4\n')
+            lines.append('module load lib/hdf5/1.12.0-openmpi-4.1-gnu-9.2\n')
+            lines.append('module load lib/cudnn/8.2-cuda-11.4\n')
+            lines.append('python svat_transport.py -b jax -d gpu -tms %s -td "${TMPDIR}" -ss %s\n' % (tms, sas_solver))
             lines.append('# Move output from local SSD to global workspace\n')
             lines.append(f'echo "Move output to {output_path_ws.as_posix()}"\n')
             lines.append('mkdir -p %s\n' % (output_path_ws.as_posix()))
