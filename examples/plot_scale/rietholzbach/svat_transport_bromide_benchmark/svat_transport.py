3efb1f15ce503c9d06f504480911c3574ce342d5
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2022-10-24 12:10:42 +0200

    rietholzbach: fixed bug in cli of bromide benchmark

--- examples/plot_scale/rietholzbach/svat_transport_bromide_benchmark/svat_transport.py
+++ examples/plot_scale/rietholzbach/svat_transport_bromide_benchmark/svat_transport.py
@@ -9,7 +9,7 @@ import click
 from roger.cli.roger_run_base import roger_base_cli
 
 
-@click.option("-tms", "--transport-model-structure", type=click.Choice(['complete-mixing', 'advection-dispersion', 'time-variant_advection-dispersion', 'power', 'time-variant_power']), default='complete-mixing')
+@click.option("-tms", "--transport-model-structure", type=click.Choice(['complete-mixing', 'piston', 'advection-dispersion', 'time-variant_advection-dispersion', 'power', 'time-variant_power']), default='complete-mixing')
 @click.option("-ss", "--sas-solver", type=click.Choice(['RK4', 'Euler', 'deterministic']), default='deterministic')
 @click.option("-td", "--tmp-dir", type=str, default=None)
 @roger_base_cli
