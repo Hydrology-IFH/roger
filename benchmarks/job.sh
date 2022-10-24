2606c7770bb747dd996f5d7c098b483b655aae27
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2022-10-24 21:07:13 +0200

    modified benchmarks

--- benchmarks/job.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
- 
-eval "$(conda shell.bash hook)"
-conda activate roger-mpi
-cd /Users/robinschwemmle/Desktop/PhD/models/roger/benchmarks
-mpirun -n 2 python oneD_benchmark.py --backend jax --device cpu -n 2 1 --size 448 448 --timesteps 5 --float-type float64
--- submit_benchmarks.sh
+++ submit_benchmarks.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 # varying problem size and fixed number of CPU cores
-# python run_benchmarks.py --sizes 1000. --sizes 10000. --sizes 100000. --sizes 1000000. --backends numpy --backends jax --backends numpy-mpi --backends jax-mpi --backends jax-gpu --nproc 25 --pmem 5000 --only oneD_benchmark.py --debug
-
-python run_benchmarks.py --sizes 10000. --backends numpy-mpi --nproc 4 --pmem 4000 --only oneD_benchmark.py --debug
+python run_benchmarks.py --sizes 1000. --sizes 10000. --sizes 100000. --sizes 1000000. --backends numpy --backends jax --backends numpy-mpi --backends jax-mpi --backends jax-gpu --nproc 25 --pmem 5000 --only oneD_benchmark.py --debug
 
 
 # fixed problem size and varying number of CPU cores
