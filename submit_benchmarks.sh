dc17a9c9404f4696b956e4559ba195573678ead7
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2022-10-25 09:32:54 +0200

    modified benchmarks

--- submit_benchmarks.sh
+++ submit_benchmarks.sh
@@ -3,7 +3,7 @@
 # varying problem size and fixed number of CPU cores
 # python run_benchmarks.py --sizes 1000. --sizes 10000. --sizes 100000. --sizes 1000000. --backends numpy --backends jax --backends numpy-mpi --backends jax-mpi --backends jax-gpu --nproc 28 --pmem 4000 --only oneD_benchmark.py --debug
 
-python run_benchmarks.py --sizes 1000. --sizes 10000. --sizes 100000. --sizes 1000000. --backends numpy-mpi  --nproc 28 --pmem 4000 --only oneD_benchmark.py --debug
+python run_benchmarks.py --sizes 10000. --backends numpy --backends numpy-mpi  --nproc 28 --pmem 4000 --only oneD_benchmark.py --debug
 
 
 # fixed problem size and varying number of CPU cores
