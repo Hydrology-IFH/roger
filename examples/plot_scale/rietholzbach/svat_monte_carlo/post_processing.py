35b2ea3c3c98bc5425b88d32495e4a81e4401592
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2022-11-02 20:41:41 +0100

    modified job scripts and post processing

--- examples/plot_scale/rietholzbach/svat_monte_carlo/post_processing.py
+++ examples/plot_scale/rietholzbach/svat_monte_carlo/post_processing.py
@@ -141,9 +141,10 @@ def main(tmp_dir):
         df_params_metrics.loc[:, 'theta_pwp'] = ds_sim["theta_pwp"].isel(y=0).values.flatten()
         df_params_metrics.loc[:, 'ks'] = ds_sim["ks"].isel(y=0).values.flatten()
         # calculate metrics
-        vars_sim = ['aet', 'q_ss', 'theta', 'dS_s', 'dS']
-        vars_obs = ['AET', 'PERC', 'THETA', 'dWEIGHT', 'dWEIGHT']
-        for var_sim, var_obs in zip(vars_sim, vars_obs):
+        vars_sim1 = ['aet', 'q_ss', 'q_ss', 'theta', 'dS_s', 'dS']
+        vars_sim = ['aet', 'q_ss', 'q_ss_corr', 'theta', 'dS_s', 'dS']
+        vars_obs = ['AET', 'PERC', 'PERC_corr', 'THETA', 'dWEIGHT', 'dWEIGHT']
+        for var_sim, var_sim1, var_obs in zip(vars_sim, vars_sim1, vars_obs):
             if var_sim == 'theta':
                 obs_vals = onp.mean(ds_obs['THETA'].isel(x=0, y=0).values, axis=0)
             elif var_sim == 'theta_rz':
@@ -156,7 +157,7 @@ def main(tmp_dir):
             df_obs.loc[:, 'obs'] = obs_vals
             for nrow in range(nx * ny):
                 for sc, sc1 in zip([0, 1, 2, 3], ['', 'dry', 'normal', 'wet']):
-                    sim_vals = ds_sim[var_sim].isel(x=nrow, y=0).values
+                    sim_vals = ds_sim[var_sim1].isel(x=nrow, y=0).values
                     # join observations on simulations
                     df_eval = eval_utils.join_obs_on_sim(date_sim, sim_vals, df_obs)
 
@@ -344,6 +345,7 @@ def main(tmp_dir):
         # Calculate multi-objective metric
         for sc, sc1 in zip([0, 1, 2, 3], ['', 'dry', 'normal', 'wet']):
             df_params_metrics.loc[:, f'E_multi{sc1}'] = 1/3 * df_params_metrics.loc[:, f'r_dS{sc1}'] + 1/3 * df_params_metrics.loc[:, f'KGE_aet{sc1}'] + 1/3 * df_params_metrics.loc[:, f'KGE_q_ss{sc1}']
+            df_params_metrics.loc[:, f'E_multi_corr{sc1}'] = 1/3 * df_params_metrics.loc[:, f'r_dS{sc1}'] + 1/3 * df_params_metrics.loc[:, f'KGE_aet{sc1}'] + 1/3 * df_params_metrics.loc[:, f'KGE_q_ss_corr{sc1}']
         # write .txt-file
         file = base_path_results / "params_metrics.txt"
         df_params_metrics.to_csv(file, header=True, index=False, sep="\t")
@@ -376,7 +378,34 @@ def main(tmp_dir):
             ax[3, 0].set_ylabel('$E_{multi}$\n [-]')
 
             fig.subplots_adjust(wspace=0.2, hspace=0.3)
-            file = base_path_figs / f"dotty_plots_{sc1}.png"
+            file = base_path_figs / f"dotty_plots_{sc1}_flags.png"
+            fig.savefig(file, dpi=250)
+            plt.close('all')
+
+            df_metrics = df_params_metrics.loc[:, [f'KGE_aet{sc1}', f'r_dS{sc1}', f'KGE_q_ss_corr{sc1}', f'E_multi_corr{sc1}']]
+            df_params = df_params_metrics.loc[:, ['dmpv', 'lmpv', 'theta_ac', 'theta_ufc', 'theta_pwp', 'ks']]
+            nrow = len(df_metrics.columns)
+            ncol = len(df_params.columns)
+            fig, ax = plt.subplots(nrow, ncol, sharey='row', figsize=(14, 7))
+            for i in range(nrow):
+                for j in range(ncol):
+                    y = df_metrics.iloc[:, i]
+                    x = df_params.iloc[:, j]
+                    ax[i, j].scatter(x, y, s=4, c='grey', alpha=0.5)
+                    ax[i, j].set_xlabel('')
+                    ax[i, j].set_ylabel('')
+
+            for j in range(ncol):
+                xlabel = labs._LABS[df_params.columns[j]]
+                ax[-1, j].set_xlabel(xlabel)
+
+            ax[0, 0].set_ylabel('$KGE_{ET}$ [-]')
+            ax[1, 0].set_ylabel(r'$r_{\Delta S}$ [-]')
+            ax[2, 0].set_ylabel('$KGE_{PERC}$ [-]')
+            ax[3, 0].set_ylabel('$E_{multi}$\n [-]')
+
+            fig.subplots_adjust(wspace=0.2, hspace=0.3)
+            file = base_path_figs / f"dotty_plots_{sc1}_noflags.png"
             fig.savefig(file, dpi=250)
             plt.close('all')
 
