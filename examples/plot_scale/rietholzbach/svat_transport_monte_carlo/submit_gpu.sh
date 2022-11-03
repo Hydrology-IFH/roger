a42016c103d25f1e52c83dd99e3c2aa6fedd0469
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2022-11-03 20:42:22 +0100

    modified job scripts and post processing

--- examples/plot_scale/rietholzbach/svat_transport_monte_carlo/submit_gpu.sh
+++ examples/plot_scale/rietholzbach/svat_transport_monte_carlo/submit_gpu.sh
@@ -1,26 +1,32 @@
 #!/bin/bash
 
 cd ~/roger/examples/plot_scale/rietholzbach/svat_transport_monte_carlo
-FILES="$PWD/oxygen18_deterministic_svat_ad_mc_*_gpu.sh"
-for f in $FILES
-do
-  qsub -q gpu $f
-done
-
-FILES="$PWD/oxygen18_deterministic_svat_adt_mc_*_gpu.sh"
-for f in $FILES
-do
-  qsub -q gpu $f
-done
-
-# FILES="$PWD/oxygen18_deterministic_svat_pf_mc_*_gpu.sh"
+# FILES="$PWD/oxygen18_deterministic_svat_ad_mc_*_gpu.sh"
 # for f in $FILES
 # do
 #   qsub -q gpu $f
 # done
 #
-# FILES="$PWD/oxygen18_deterministic_svat_tv_mc_*_gpu.sh"
+# FILES="$PWD/oxygen18_deterministic_svat_adt_mc_*_gpu.sh"
 # for f in $FILES
 # do
 #   qsub -q gpu $f
 # done
+
+FILES="$PWD/oxygen18_deterministic_svat_pf_mc_*_gpu.sh"
+for f in $FILES
+do
+  qsub -q gpu $f
+done
+
+FILES="$PWD/oxygen18_deterministic_svat_pow_mc_*_gpu.sh"
+for f in $FILES
+do
+  qsub -q gpu $f
+done
+
+FILES="$PWD/oxygen18_deterministic_svat_tv_mc_*_gpu.sh"
+for f in $FILES
+do
+  qsub -q gpu $f
+done
