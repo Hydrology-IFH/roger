44592cb472a8b0256c707a7dec79a84b322be68b
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2022-11-05 20:59:23 +0100

    modified jon scripts

--- examples/plot_scale/rietholzbach/svat/svat.sh
+++ examples/plot_scale/rietholzbach/svat/svat.sh
@@ -11,11 +11,4 @@ conda activate roger
 cd /home/fr/fr_fr/fr_rs1092/roger/examples/plot_scale/rietholzbach/svat
 
 # adapt command to your available scheduler / MPI implementation
-python svat.py -b numpy -d cpu -td "${TMPDIR}"
-
-# Write output to temporary SSD of computing node
-echo "Write output to $TMPDIR"
-# Move output from temporary SSD to workspace
-echo "Move output to /beegfs/work/workspace/ws/fr_rs1092-workspace-0/rietholzbach/svat"
-mkdir -p /beegfs/work/workspace/ws/fr_rs1092-workspace-0/rietholzbach/svat
-mv "${TMPDIR}"/*.nc /beegfs/work/workspace/ws/fr_rs1092-workspace-0/rietholzbach/svat
+python svat.py -b numpy -d cpu
