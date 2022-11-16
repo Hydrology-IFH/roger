2b9cfc437382da903ab2bdf28d24032e990915e9
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2022-11-16 13:51:30 +0100

    modified job scripts

--- submit_benchmarks_var_proc.sh
+++ submit_benchmarks_var_proc.sh
@@ -17,7 +17,7 @@
 # python run_benchmarks.py --sizes 1000000. --backends numpy-mpi --backends jax-mpi --nproc 112 --pmem 4000 --only oneD_benchmark.py --debug
 # python run_benchmarks.py --sizes 1000000. --backends numpy-mpi --backends jax-mpi --nproc 224 --pmem 4000 --only oneD_benchmark.py --debug
 
-python run_benchmarks.py --sizes 100000. --backends numpy-mpi --backends jax-mpi --nproc 4 --pmem 32000 --only SVATOXYGEN18_benchmark.py --timesteps 20 --debug
-python run_benchmarks.py --sizes 100000. --backends numpy-mpi --backends jax-mpi --nproc 8 --pmem 32000 --only SVATOXYGEN18_benchmark.py --timesteps 20 --debug
-python run_benchmarks.py --sizes 100000. --backends numpy-mpi --backends jax-mpi --nproc 16 --pmem 32000 --only SVATOXYGEN18_benchmark.py --timesteps 20 --debug
-python run_benchmarks.py --sizes 100000. --backends numpy-mpi --backends jax-mpi --nproc 28 --pmem 32000 --only SVATOXYGEN18_benchmark.py --timesteps 20 --debug
+python run_benchmarks.py --sizes 10000. --backends numpy-mpi --backends jax-mpi --nproc 4 --pmem 32000 --only SVATOXYGEN18_benchmark.py --timesteps 20 --debug
+python run_benchmarks.py --sizes 10000. --backends numpy-mpi --backends jax-mpi --nproc 8 --pmem 16000 --only SVATOXYGEN18_benchmark.py --timesteps 20 --debug
+python run_benchmarks.py --sizes 10000. --backends numpy-mpi --backends jax-mpi --nproc 16 --pmem 8000 --only SVATOXYGEN18_benchmark.py --timesteps 20 --debug
+python run_benchmarks.py --sizes 10000. --backends numpy-mpi --backends jax-mpi --nproc 28 --pmem 4000 --only SVATOXYGEN18_benchmark.py --timesteps 20 --debug
