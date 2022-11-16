f339d4b23392806234a96c48658cc23091cc9851
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2022-11-16 21:27:14 +0100

    modified job scripts

--- submit_benchmarks_var_proc.sh
+++ submit_benchmarks_var_proc.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # fixed problem size and varying number of CPU cores
-# python run_benchmarks.py --sizes 1000000. --backends numpy-mpi --backends jax-mpi --nproc 4 --pmem 32000 --only SVAT_benchmark.py --debug
-# python run_benchmarks.py --sizes 1000000. --backends numpy-mpi --backends jax-mpi --nproc 8 --pmem 16000 --only SVAT_benchmark.py --debug
-# python run_benchmarks.py --sizes 1000000. --backends numpy-mpi --backends jax-mpi --nproc 16 --pmem 8000 --only SVAT_benchmark.py --debug
-# python run_benchmarks.py --sizes 1000000. --backends numpy-mpi --backends jax-mpi --nproc 28 --pmem 4000 --only SVAT_benchmark.py --debug
-# python run_benchmarks.py --sizes 1000000. --backends numpy-mpi --backends jax-mpi --nproc 56 --pmem 4000 --only SVAT_benchmark.py --debug
-# python run_benchmarks.py --sizes 1000000. --backends numpy-mpi --backends jax-mpi --nproc 112 --pmem 4000 --only SVAT_benchmark.py --debug
-# python run_benchmarks.py --sizes 1000000. --backends numpy-mpi --backends jax-mpi --nproc 224 --pmem 4000 --only SVAT_benchmark.py --debug
+python run_benchmarks.py --sizes 1000000. --backends numpy-mpi --backends jax-mpi --nproc 4 --pmem 32000 --only SVAT_benchmark.py --debug
+python run_benchmarks.py --sizes 1000000. --backends numpy-mpi --backends jax-mpi --nproc 8 --pmem 16000 --only SVAT_benchmark.py --debug
+python run_benchmarks.py --sizes 1000000. --backends numpy-mpi --backends jax-mpi --nproc 16 --pmem 8000 --only SVAT_benchmark.py --debug
+python run_benchmarks.py --sizes 1000000. --backends numpy-mpi --backends jax-mpi --nproc 28 --pmem 4000 --only SVAT_benchmark.py --debug
+python run_benchmarks.py --sizes 1000000. --backends numpy-mpi --backends jax-mpi --nproc 56 --pmem 4000 --only SVAT_benchmark.py --debug
+python run_benchmarks.py --sizes 1000000. --backends numpy-mpi --backends jax-mpi --nproc 112 --pmem 4000 --only SVAT_benchmark.py --debug
+python run_benchmarks.py --sizes 1000000. --backends numpy-mpi --backends jax-mpi --nproc 224 --pmem 4000 --only SVAT_benchmark.py --debug
 #
 # python run_benchmarks.py --sizes 1000000. --backends numpy-mpi --backends jax-mpi --nproc 4 --pmem 32000 --only oneD_benchmark.py --debug
 # python run_benchmarks.py --sizes 1000000. --backends numpy-mpi --backends jax-mpi --nproc 8 --pmem 16000 --only oneD_benchmark.py --debug
@@ -17,7 +17,7 @@
 # python run_benchmarks.py --sizes 1000000. --backends numpy-mpi --backends jax-mpi --nproc 112 --pmem 4000 --only oneD_benchmark.py --debug
 # python run_benchmarks.py --sizes 1000000. --backends numpy-mpi --backends jax-mpi --nproc 224 --pmem 4000 --only oneD_benchmark.py --debug
 
-python run_benchmarks.py --sizes 20000. --backends numpy-mpi --backends jax-mpi --nproc 4 --pmem 32000 --only SVATOXYGEN18_benchmark.py --timesteps 20 --debug
-python run_benchmarks.py --sizes 20000. --backends numpy-mpi --backends jax-mpi --nproc 8 --pmem 16000 --only SVATOXYGEN18_benchmark.py --timesteps 20 --debug
-python run_benchmarks.py --sizes 20000. --backends numpy-mpi --backends jax-mpi --nproc 16 --pmem 8000 --only SVATOXYGEN18_benchmark.py --timesteps 20 --debug
-python run_benchmarks.py --sizes 20000. --backends numpy-mpi --backends jax-mpi --nproc 28 --pmem 4000 --only SVATOXYGEN18_benchmark.py --timesteps 20 --debug
+# python run_benchmarks.py --sizes 20000. --backends numpy-mpi --backends jax-mpi --nproc 4 --pmem 32000 --only SVATOXYGEN18_benchmark.py --timesteps 20 --debug
+# python run_benchmarks.py --sizes 20000. --backends numpy-mpi --backends jax-mpi --nproc 8 --pmem 16000 --only SVATOXYGEN18_benchmark.py --timesteps 20 --debug
+# python run_benchmarks.py --sizes 20000. --backends numpy-mpi --backends jax-mpi --nproc 16 --pmem 8000 --only SVATOXYGEN18_benchmark.py --timesteps 20 --debug
+# python run_benchmarks.py --sizes 20000. --backends numpy-mpi --backends jax-mpi --nproc 28 --pmem 4000 --only SVATOXYGEN18_benchmark.py --timesteps 20 --debug
