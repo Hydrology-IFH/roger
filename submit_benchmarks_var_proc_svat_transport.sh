6aedaf8c5ec4d0c88925b78039134dbf6fb28671
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2022-11-18 15:21:26 +0100

    modified benchmarks

---
+++ submit_benchmarks_var_proc_svat_transport.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# fixed problem size and varying number of CPU cores
+python run_benchmarks.py --sizes 20000. --backends numpy-mpi --backends jax-mpi --nproc 4 --pmem 32000 --only SVATOXYGEN18_benchmark.py --timesteps 20 --debug
+python run_benchmarks.py --sizes 20000. --backends numpy-mpi --backends jax-mpi --nproc 8 --pmem 16000 --only SVATOXYGEN18_benchmark.py --timesteps 20 --debug
+python run_benchmarks.py --sizes 20000. --backends numpy-mpi --backends jax-mpi --nproc 16 --pmem 8000 --only SVATOXYGEN18_benchmark.py --timesteps 20 --debug
+python run_benchmarks.py --sizes 20000. --backends numpy-mpi --backends jax-mpi --nproc 28 --pmem 4000 --only SVATOXYGEN18_benchmark.py --timesteps 20 --debug
---
+++ submit_benchmarks_var_size_svat.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# varying problem size and fixed number of CPU cores
+python run_benchmarks.py --sizes 1000. --sizes 10000. --sizes 100000. --sizes 500000. --sizes 1000000. --sizes 2000000. --backends numpy --backends jax --backends numpy-mpi --backends jax-mpi --backends jax-gpu --nproc 28 --pmem 4000 --only SVAT_benchmark.py --debug
