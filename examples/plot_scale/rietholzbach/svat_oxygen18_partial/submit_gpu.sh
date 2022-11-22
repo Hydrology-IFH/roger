ca588d7e1c26da5f9721e65d49c0e19f07b3ede0
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2022-11-22 18:18:52 +0100

    modified job scripts

--- examples/plot_scale/rietholzbach/svat_oxygen18_partial/submit_gpu.sh
+++ examples/plot_scale/rietholzbach/svat_oxygen18_partial/submit_gpu.sh
@@ -1,49 +1,7 @@
 #!/bin/bash
 
 cd ~/roger/examples/plot_scale/rietholzbach/svat_oxygen18_partial
-FILES="$PWD/oxygen18_deterministic_svat_ad_partial_*_gpu.sh"
-for f in $FILES
-do
-  qsub -q gpu $f
-done
-
-FILES="$PWD/oxygen18_deterministic_svat_adt_partial_*_gpu.sh"
-for f in $FILES
-do
-  qsub -q gpu $f
-done
-
-FILES="$PWD/oxygen18_deterministic_svat_pf_partial_*_gpu.sh"
-for f in $FILES
-do
-  qsub -q gpu $f
-done
-
-FILES="$PWD/oxygen18_deterministic_svat_pfad_partial_*_gpu.sh"
-for f in $FILES
-do
-  qsub -q gpu $f
-done
-
-FILES="$PWD/oxygen18_deterministic_svat_op_partial_*_gpu.sh"
-for f in $FILES
-do
-  qsub -q gpu $f
-done
-
-FILES="$PWD/oxygen18_deterministic_svat_pow_partial_*_gpu.sh"
-for f in $FILES
-do
-  qsub -q gpu $f
-done
-
-FILES="$PWD/oxygen18_deterministic_svat_tvt_partial_*_gpu.sh"
-for f in $FILES
-do
-  qsub -q gpu $f
-done
-
-FILES="$PWD/oxygen18_deterministic_svat_tv_partial_*_gpu.sh"
+FILES="$PWD/oxygen18_deterministic_svat_*_partial_gpu.sh"
 for f in $FILES
 do
   qsub -q gpu $f
