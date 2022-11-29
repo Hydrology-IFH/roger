613a901b73cdcc86d3e8a763f223c70647e225c7
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2022-11-29 11:48:48 +0100

    modified job scripts

--- examples/plot_scale/rietholzbach/svat_oxygen18_partial/submit_gpu.sh
+++ examples/plot_scale/rietholzbach/svat_oxygen18_partial/submit_gpu.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 cd ~/roger/examples/plot_scale/rietholzbach/svat_oxygen18_partial
-FILES="$PWD/oxygen18_deterministic_svat_*_partial_gpu.sh"
-for f in $FILES
-do
-  qsub -q gpu $f
-done
+# FILES="$PWD/oxygen18_deterministic_svat_*_partial_gpu.sh"
+# for f in $FILES
+# do
+#   qsub -q gpu $f
+# done
 
 FILES="$PWD/oxygen18_RK4_svat_*_partial_gpu.sh"
 for f in $FILES
