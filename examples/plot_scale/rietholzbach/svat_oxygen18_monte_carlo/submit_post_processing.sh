c85e6c02f9b3422d8e7bdd6d764b228f2c9ebcdb
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2022-12-07 09:38:17 +0100

    modified post processing

---
+++ examples/plot_scale/rietholzbach/svat_oxygen18_monte_carlo/submit_post_processing.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd ~/roger/examples/plot_scale/rietholzbach/svat_oxygen18_monte_carlo
+
+qsub -q short post_processing_cm.sh
+qsub -q short post_processing_pi.sh
+qsub -q short post_processing_ad.sh
+qsub -q short post_processing_adt.sh
+qsub -q short post_processing_pow.sh
+qsub -q short post_processing_powt.sh
+qsub -q short post_processing_pfad.sh
+qsub -q short post_processing_pfadt.sh
+qsub -q short post_processing_tvt.sh
+qsub -q short post_processing_tv.sh
