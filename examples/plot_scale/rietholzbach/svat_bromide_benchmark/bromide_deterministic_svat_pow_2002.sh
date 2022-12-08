ff922d28e0c27219aa590542fad4bc53c1333580
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2022-12-08 15:27:53 +0100

    modified job scripts

---
+++ examples/plot_scale/rietholzbach/svat_bromide_benchmark/bromide_deterministic_svat_pow_2002.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#PBS -l nodes=1:ppn=1
+#PBS -l walltime=8:00:00
+#PBS -l pmem=4000mb
+#PBS -N bromide_deterministic_svat_pow_2002
+ 
+# load module dependencies
+module load lib/hdf5/1.12.0-openmpi-4.1-gnu-9.2
+export OMP_NUM_THREADS=1
+eval "$(conda shell.bash hook)"
+conda activate roger-mpi
+cd /home/fr/fr_fr/fr_rs1092/roger/examples/plot_scale/rietholzbach/svat_bromide_benchmark
+ 
+python svat_transport.py -b jax -d cpu -tms power -td "${TMPDIR}" -ss deterministic -y 2002
+# Move output from local SSD to global workspace
+echo "Move output to /beegfs/work/workspace/ws/fr_rs1092-workspace-0/rietholzbach/svat_bromide_benchmark"
+mkdir -p /beegfs/work/workspace/ws/fr_rs1092-workspace-0/rietholzbach/svat_bromide_benchmark
+mv "${TMPDIR}"/*.nc /beegfs/work/workspace/ws/fr_rs1092-workspace-0/rietholzbach/svat_bromide_benchmark
