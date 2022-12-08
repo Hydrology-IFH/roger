d8f06e83da2064736ec80af1194ce2aacbfe0919
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2022-12-08 15:35:41 +0100

    modified job scripts

--- examples/plot_scale/rietholzbach/svat_bromide_benchmark/submit.sh
+++ examples/plot_scale/rietholzbach/svat_bromide_benchmark/submit.sh
@@ -1,8 +1,38 @@
 #!/bin/bash
 
 cd ~/roger/examples/plot_scale/rietholzbach/svat_transport_bromide_benchmark
-FILES="$PWD/bromide_deterministic_svat_*_*.sh"
+FILES="$PWD/bromide_deterministic_svat_cm_*.sh"
 for f in $FILES
 do
   qsub -q short $f
 done
+
+FILES="$PWD/bromide_deterministic_svat_pi_*.sh"
+for f in $FILES
+do
+  qsub -q short $f
+done
+
+FILES="$PWD/bromide_deterministic_svat_ad_*.sh"
+for f in $FILES
+do
+  qsub -q short $f
+done
+
+FILES="$PWD/bromide_deterministic_svat_adt_*.sh"
+for f in $FILES
+do
+  qsub -q short $f
+done
+
+FILES="$PWD/bromide_deterministic_svat_pow_*.sh"
+for f in $FILES
+do
+  qsub -q short $f
+done
+
+# FILES="$PWD/bromide_deterministic_svat_powt_*.sh"
+# for f in $FILES
+# do
+#   qsub -q short $f
+# done
