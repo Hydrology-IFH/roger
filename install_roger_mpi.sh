6a59f065d6fa3229b47702c1972e43eb64054d46
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2023-05-11 11:52:41 +0200

    freiburg_altheim_kupferzell: modified job scripts

--- install_roger_mpi.sh
+++ install_roger_mpi.sh
@@ -2,6 +2,6 @@
 # requires Open MPI to be installed
 # requires parallel HDF5 to be installed
 pip install mpi4py --no-binary mpi4py
-CC=mpicc HDF5_MPI="ON" HDF5_DIR=/usr/local/hdf5 pip install --no-binary=h5py h5py==3.7.0 --no-build-isolation
+CC=mpicc HDF5_MPI="ON" HDF5_DIR=/usr/local/hdf5 pip install --no-binary=h5py h5py==3.8.0 --no-build-isolation
 pip install h5netcdf --no-build-isolation
 pip install mpi4jax --no-build-isolation
