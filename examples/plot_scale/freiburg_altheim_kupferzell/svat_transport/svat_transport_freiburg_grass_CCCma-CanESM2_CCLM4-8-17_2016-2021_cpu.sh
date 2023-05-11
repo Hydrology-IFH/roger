8607c7f08ebd5fa1d2a1a6a436861f62dcefdcba
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2023-05-11 12:01:23 +0200

    freiburg_altheim_kupferzell: modified job scripts

--- examples/plot_scale/freiburg_altheim_kupferzell/svat_transport/svat_transport_freiburg_grass_CCCma-CanESM2_CCLM4-8-17_2016-2021_cpu.sh
+++ examples/plot_scale/freiburg_altheim_kupferzell/svat_transport/svat_transport_freiburg_grass_CCCma-CanESM2_CCLM4-8-17_2016-2021_cpu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -l nodes=1:ppn=1
 #PBS -l walltime=24:00:00
-#PBS -l pmem=4000mb
+#PBS -l pmem=8000mb
 #PBS -N svat_transport_freiburg_grass_CCCma-CanESM2_CCLM4-8-17_2016-2021
 #PBS -m a
 #PBS -M robin.schwemmle@hydrology.uni-freiburg.de
