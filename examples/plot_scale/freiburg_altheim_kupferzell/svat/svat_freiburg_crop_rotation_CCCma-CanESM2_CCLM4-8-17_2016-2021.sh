e7235144898fd60911fd0cb82f55a757d70879ac
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2023-05-23 17:37:45 +0200

    freiburg_altheim_kupferzell: modified job scripts.

--- examples/plot_scale/freiburg_altheim_kupferzell/svat/svat_freiburg_crop_rotation_CCCma-CanESM2_CCLM4-8-17_2016-2021.sh
+++ examples/plot_scale/freiburg_altheim_kupferzell/svat/svat_freiburg_crop_rotation_CCCma-CanESM2_CCLM4-8-17_2016-2021.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #PBS -l nodes=1:ppn=1
-#PBS -l walltime=24:00:00
+#PBS -l walltime=6:00:00
 #PBS -l pmem=80000mb
 #PBS -N svat_freiburg_crop_rotation_CCCma-CanESM2_CCLM4-8-17_2016-2021
 #PBS -m a
