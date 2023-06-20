6c49b73025fa040673da10e55810c02f1423dd69
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2023-06-20 17:19:11 +0200

    modified mpi job scripts for binac cluster

--- install_roger_gpu_on_binac.sh
+++ install_roger_gpu_on_binac.sh
@@ -8,6 +8,6 @@ pip cache purge
 # double-check whether open MPI has CUDA support
 ompi_info --parsable --all | grep mpi_built_with_cuda_support:value
 pip install mpi4py --no-binary mpi4py
-HDF5_VERSION=1.12.0 CC="mpicc" HDF5_MPI="ON" HDF5_DIR=/opt/bwhpc/common/lib/hdf5/1.12.0-openmpi-4.1-gnu-9.2 pip install --no-binary=h5py h5py==3.7.0
+HDF5_VERSION=1.12.0 CC="mpicc" HDF5_MPI="ON" HDF5_DIR=/opt/bwhpc/common/lib/hdf5/1.12.0-openmpi-4.1-gnu-9.2 pip install --no-binary=h5py h5py==3.9.0
 pip install h5netcdf --no-build-isolation
 CUDA_ROOT=/opt/bwhpc/common/devel/cuda/11.4 pip install mpi4jax --no-build-isolation
