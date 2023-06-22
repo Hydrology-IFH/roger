2bdff30c850ecde91902a5fac0aafe0cdc6077ef
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2023-06-22 18:17:37 +0200

    modified job scripts

--- examples/plot_scale/freiburg_altheim_kupferzell/submit_svat_corn_catch_crop_transport_jobs_slurm.sh
+++ examples/plot_scale/freiburg_altheim_kupferzell/submit_svat_corn_catch_crop_transport_jobs_slurm.sh
@@ -2,7 +2,7 @@
 
 cd ~/roger/examples/plot_scale/freiburg_altheim_kupferzell/svat_transport
 
-FILES="$PWD/svat_transport_*_corn_catch_crop_*_cpu_slurm.sh"
+FILES="$PWD/svat_transport_*_corn_catch_crop_*_cpumpi_slurm.sh"
 for f in $FILES
 do
   sbatch --partition=single $f
