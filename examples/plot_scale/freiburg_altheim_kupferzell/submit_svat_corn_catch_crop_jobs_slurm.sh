6eee24985fcf5faad1aeb1aff5407bcdeda0e61b
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2023-06-22 14:40:45 +0200

    modified job scripts

---
+++ examples/plot_scale/freiburg_altheim_kupferzell/submit_svat_corn_catch_crop_jobs_slurm.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd ~/roger/examples/plot_scale/freiburg_altheim_kupferzell/svat
+
+FILES="$PWD/svat_*_corn_catch_crop_*_slurm.sh"
+for f in $FILES
+do
+  sbatch --partition=single $f
+done
---
+++ examples/plot_scale/freiburg_altheim_kupferzell/submit_svat_grass_transport_jobs_slurm.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd ~/roger/examples/plot_scale/freiburg_altheim_kupferzell/svat_transport
+
+FILES="$PWD/svat_transport_*_grass_*_cpumpi_slurm.sh"
+for f in $FILES
+do
+  sbatch --partition=single $f
+done
+
