4026cdf5b87e955b5d0b1b1a641c8fd001ad91f4
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2023-09-12 16:14:39 +0200

    modified examples

--- examples/plot_scale/reckenholz/svat_monte_carlo/dotty_plots.py
+++ examples/plot_scale/reckenholz/svat_monte_carlo/dotty_plots.py
@@ -29,22 +29,43 @@ def main(tmp_dir):
 
     lys_experiments = ["lys2", "lys3", "lys4", "lys8", "lys9"]
     for lys_experiment in lys_experiments:
-        df_params_eff = pd.read_csv(base_path_output / f"params_eff_{lys_experiment}.txt", sep="\t")
+        df_params_metrics = pd.read_csv(base_path_output / f"params_eff_{lys_experiment}.txt", sep="\t")
+        df_params_metrics = df_params_metrics.sort_values(by=['E_multi'], ascending=False)
+        df_params_metrics.loc[:, 'id'] = range(len(df_params_metrics.index))
+        idx_best100 = df_params_metrics.loc[:df_params_metrics.index[99], 'id'].values.tolist()
 
-        df_eff = df_params_eff.loc[:, ["KGE_q_ss", "r_theta", "E_multi"]]
-        df_params = df_params_eff.loc[:, ["theta_ac", "theta_ufc", "theta_pwp", "ks"]]
-        nrow = len(df_eff.columns)
+        df_metrics = df_params_metrics.loc[:, ["KGE_q_ss", "r_theta", "E_multi"]]
+        df_params = df_params_metrics.loc[:, ["theta_ac", "theta_ufc", "theta_pwp", "ks"]]
+        nrow = len(df_metrics.columns)
         ncol = len(df_params.columns)
-        fig, ax = plt.subplots(nrow, ncol, sharey=True, figsize=(6, 4.5))
-        for i in range(nrow):
-            for j in range(ncol):
-                y = df_eff.iloc[:, i]
+        fig, ax = plt.subplots(nrow, ncol, sharey='row', figsize=(6, 4.5))
+        for i, metric_var in enumerate(df_metrics.columns):
+            for j, param in enumerate(df_params.columns):
+                y = df_metrics.iloc[:, i]
                 x = df_params.iloc[:, j]
-                sns.regplot(
-                    x=x, y=y, ax=ax[i, j], ci=None, color="k", scatter_kws={"alpha": 0.2, "s": 4, "color": "grey"}
+                ax[i, j].scatter(
+                    x, y, alpha=0.2, s=4, color="grey"
                 )
                 ax[i, j].set_xlabel("")
                 ax[i, j].set_ylabel("")
+                if metric_var in ['KGE_q_ss']:
+                    ax[i, j].set_ylim((0.2, 0.7))
+                elif metric_var in ['r_theta']:
+                    ax[i, j].set_ylim((0.5, 1.0))
+                elif metric_var in ['E_multi']:
+                    ax[i, j].set_ylim((0.5, 0.8))
+
+                # best parameter set for individual evaluation metric at specific storage conditions
+                df_params_metrics_sc1 = df_params_metrics.copy()
+                df_params_metrics_sc1 = df_params_metrics_sc1.sort_values(by=[df_metrics.columns[i]], ascending=False)
+                idx_best_sc1 = df_params_metrics_sc1.loc[:df_params_metrics_sc1.index[99], 'id'].values.tolist()
+                for idx_best_sc in idx_best_sc1:
+                    y_best_sc = df_metrics.iloc[idx_best_sc, i]
+                    x_best_sc = df_params.iloc[idx_best_sc, j]
+                    ax[i, j].scatter(x_best_sc, y_best_sc, s=2, color='blue', alpha=0.8)
+                if metric_var in ['KGE_q_ss', 'theta_r']:
+                    # best parameter sets for multi-objective criteria
+                    ax[i, j].scatter(x[:100], y[:100], s=2, color='red', alpha=1)
 
         for j in range(ncol):
             xlabel = labs._LABS[df_params.columns[j]]
@@ -58,20 +79,35 @@ def main(tmp_dir):
         file = base_path_figs / f"dotty_plots_swr_{lys_experiment}.png"
         fig.savefig(file, dpi=250)
 
-        df_params = df_params_eff.loc[:, ["dmpv", "lmpv"]]
-        nrow = len(df_eff.columns)
+        df_params = df_params_metrics.loc[:, ["dmpv", "lmpv"]]
+        nrow = len(df_metrics.columns)
         ncol = len(df_params.columns)
-        fig, ax = plt.subplots(nrow, ncol, sharey=True, figsize=(3, 4.5))
-        for i in range(nrow):
-            for j in range(ncol):
-                y = df_eff.iloc[:, i]
+        fig, ax = plt.subplots(nrow, ncol, sharey='row', figsize=(3, 4.5))
+        for i, metric_var in enumerate(df_metrics.columns):
+            for j, param in enumerate(df_params.columns):
+                y = df_metrics.iloc[:, i]
                 x = df_params.iloc[:, j]
-                sns.regplot(
-                    x=x, y=y, ax=ax[i, j], ci=None, color="k", scatter_kws={"alpha": 0.2, "s": 4, "color": "grey"}
-                )
+                ax[i, j].scatter(x, y, color='grey', alpha=0.2, s=4)
                 ax[i, j].set_xlabel("")
                 ax[i, j].set_ylabel("")
+                if metric_var in ['KGE_q_ss']:
+                    ax[i, j].set_ylim((0.2, 0.7))
+                elif metric_var in ['r_theta']:
+                    ax[i, j].set_ylim((0.5, 1.0))
+                elif metric_var in ['E_multi']:
+                    ax[i, j].set_ylim((0.5, 0.8))
 
+                # best parameter set for individual evaluation metric at specific storage conditions
+                df_params_metrics_sc1 = df_params_metrics.copy()
+                df_params_metrics_sc1 = df_params_metrics_sc1.sort_values(by=[df_metrics.columns[i]], ascending=False)
+                idx_best_sc1 = df_params_metrics_sc1.loc[:df_params_metrics_sc1.index[99], 'id'].values.tolist()
+                for idx_best_sc in idx_best_sc1:
+                    y_best_sc = df_metrics.iloc[idx_best_sc, i]
+                    x_best_sc = df_params.iloc[idx_best_sc, j]
+                    ax[i, j].scatter(x_best_sc, y_best_sc, s=2, color='blue', alpha=0.8)
+                if metric_var in ['KGE_q_ss', 'theta_r']:
+                    # best parameter sets for multi-objective criteria
+                    ax[i, j].scatter(x[:100], y[:100], s=2, color='red', alpha=1)
         for j in range(ncol):
             xlabel = labs._LABS[df_params.columns[j]]
             ax[-1, j].set_xlabel(xlabel)
