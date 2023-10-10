022e657e4ffb2a07229cfc22bef6fda1a0b795b5
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2023-10-10 15:54:54 +0200

    modified examples

---
+++ examples/plot_scale/boadkh/svat_crop/svat_crop_heidelsheim_summer-wheat_clover_winter-wheat_slurm.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --time=2:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=4000
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-user=robin.schwemmle@hydrology.uni-freiburg.de
+#SBATCH --job-name=svat_crop_heidelsheim_summer-wheat_clover_winter-wheat
+#SBATCH --output=svat_crop_heidelsheim_summer-wheat_clover_winter-wheat.out
+#SBATCH --error=svat_crop_heidelsheim_summer-wheat_clover_winter-wheat_err.out
+#SBATCH --export=ALL
+ 
+eval "$(conda shell.bash hook)"
+conda activate roger
+cd /home/fr/fr_fr/fr_rs1092/roger/examples/plot_scale/boadkh/svat_crop
+ 
+python svat_crop.py -b numpy -d cpu --location heidelsheim --crop-rotation-scenario summer-wheat_clover_winter-wheat -td "${TMPDIR}"
+# Move output from local SSD to global workspace
+echo "Move output to /home/fr/fr_fr/fr_rs1092/roger/examples/plot_scale/boadkh/output/svat_crop"
+mkdir -p /home/fr/fr_fr/fr_rs1092/roger/examples/plot_scale/boadkh/output/svat_crop
+mv "${TMPDIR}"/SVATCROP_*.nc /home/fr/fr_fr/fr_rs1092/roger/examples/plot_scale/boadkh/output/svat_crop
