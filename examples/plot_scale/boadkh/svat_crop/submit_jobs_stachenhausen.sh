ca6df1959614654b693f4ef1508e1eef75f9f5db
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2023-10-11 09:59:42 +0200

    modfied examples

--- examples/plot_scale/boadkh/svat_crop/submit_jobs_stachenhausen.sh
+++ examples/plot_scale/boadkh/svat_crop/submit_jobs_stachenhausen.sh
@@ -2,7 +2,7 @@
 
 cd ~/roger/examples/plot_scale/boadkh/svat_crop
 
-FILES="$PWD/svat_crop_altheim_*_slurm.sh"
+FILES="$PWD/svat_crop_stachenhausen_*_slurm.sh"
 for f in $FILES
 do
   sbatch --partition=single $f
