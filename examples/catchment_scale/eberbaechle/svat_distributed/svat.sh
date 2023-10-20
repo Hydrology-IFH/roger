4fe54cce6338d56c4b4906826204b2ebb2207f2f
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2023-10-20 09:44:33 +0200

    modified examples

--- examples/catchment_scale/eberbaechle/svat_distributed/svat.sh
+++ examples/catchment_scale/eberbaechle/svat_distributed/svat.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
-#PBS -l nodes=1:ppn=1
-#PBS -l walltime=4:00:00
-#PBS -l pmem=4000mb
-#PBS -N svat
-#PBS -m bea
-#PBS -M robin.schwemmle@hydrology.uni-freiburg.de
-
+#SBATCH --time=6:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=4000
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-user=robin.schwemmle@hydrology.uni-freiburg.de
+#SBATCH --job-name=svat18O
+#SBATCH --output=svat18O.out
+#SBATCH --error=svat18O_err.out
+#SBATCH --export=ALL
+ 
 eval "$(conda shell.bash hook)"
 conda activate roger
-cd /home/fr/fr_fr/fr_rs1092/roger/examples/catchment_scale/ruetlibach/svat_distributed
-
-# adapt command to your available scheduler / MPI implementation
-python svat.py -b numpy -d cpu -td "${TMPDIR}"
-
-# Write output to temporary SSD of computing node
-echo "Write output to $TMPDIR"
-# Move output from temporary SSD to workspace
-echo "Move output to /beegfs/work/workspace/ws/fr_rs1092-workspace-0/svat_distributed_tutorial"
-mkdir -p /beegfs/work/workspace/ws/fr_rs1092-workspace-0/svat_distributed_tutorial
-mv "${TMPDIR}"/*.nc /beegfs/work/workspace/ws/fr_rs1092-workspace-0/svat_distributed_tutorial
+cd /home/fr/fr_fr/fr_rs1092/roger/examples/catchment_scale/eberbaechle/svat_oxygen18_distributed
+  
+python svat.py -td "${TMPDIR}"
+# Move output from local SSD to global workspace
+echo "Move output to /home/fr/fr_fr/fr_rs1092/roger/examples/catchment_scale/eberbaechle/svat_distributed/output"
+mkdir -p /home/fr/fr_fr/fr_rs1092/roger/examples/catchment_scale/eberbaechle/svat_distributed/output
+mv "${TMPDIR}"/SVAT.*.nc /home/fr/fr_fr/fr_rs1092/roger/examples/catchment_scale/eberbaechle/svat_distributed/output
