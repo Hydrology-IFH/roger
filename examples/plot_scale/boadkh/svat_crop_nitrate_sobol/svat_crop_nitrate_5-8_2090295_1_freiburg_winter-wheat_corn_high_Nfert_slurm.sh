6d83e908666303126e46721a5c80e5114e1ab8b6
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2024-02-05 15:32:57 +0100

    modified examples

---
+++ examples/plot_scale/boadkh/svat_crop_nitrate_sobol/svat_crop_nitrate_5-8_2090295_1_freiburg_winter-wheat_corn_high_Nfert_slurm.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#SBATCH --time=12:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=32
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=16000
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-user=robin.schwemmle@hydrology.uni-freiburg.de
+#SBATCH --job-name=svat_crop_nitrate_5-8_2090295_1_freiburg_winter-wheat_corn_high_Nfert
+#SBATCH --output=svat_crop_nitrate_5-8_2090295_1_freiburg_winter-wheat_corn_high_Nfert.out
+#SBATCH --error=svat_crop_nitrate_5-8_2090295_1_freiburg_winter-wheat_corn_high_Nfert_err.out
+#SBATCH --export=ALL
+ 
+# load module dependencies
+module load devel/cuda/10.2
+module load devel/cudnn/10.2
+module load lib/hdf5/1.12.2-gnu-12.1-openmpi-4.1
+# prevent memory issues for Open MPI 4.1.x
+export OMPI_MCA_btl="self,smcuda,vader,tcp"
+export OMP_NUM_THREADS=1
+eval "$(conda shell.bash hook)"
+conda activate roger-mpi
+cd /home/fr/fr_fr/fr_rs1092/roger/examples/plot_scale/boadkh/svat_crop_nitrate_sobol
+ 
+# Copy fluxes and states from global workspace to local SSD
+echo "Copy fluxes and states from global workspace to local SSD"
+# Compares hashes
+checksum_gws=$(shasum -a 256 /home/fr/fr_fr/fr_rs1092/roger/examples/plot_scale/boadkh/output/svat_crop/SVATCROP_5-8_2090295_1_freiburg_winter-wheat_corn.nc | cut -f 1 -d " ")
+checksum_ssd=0a
+cp /home/fr/fr_fr/fr_rs1092/roger/examples/plot_scale/boadkh/output/svat_crop/SVATCROP_5-8_2090295_1_freiburg_winter-wheat_corn.nc "${TMPDIR}"
+# Wait for termination of moving files
+while [ "${checksum_gws}" != "${checksum_ssd}" ]; do
+sleep 10
+checksum_ssd=$(shasum -a 256 "${TMPDIR}"/SVATCROP_5-8_2090295_1_freiburg_winter-wheat_corn.nc | cut -f 1 -d " ")
+done
+echo "Copying was successful"
+ 
+python svat_crop_nitrate.py -b jax -d cpu --float-type float64 --row 0 --id 5-8_2090295_1 --location freiburg --crop-rotation-scenario winter-wheat_corn --fertilization-intensity high -td "${TMPDIR}"
+# Move output from local SSD to global workspace
+echo "Move output to /home/fr/fr_fr/fr_rs1092/roger/examples/plot_scale/boadkh/output/svat_crop_nitrate"
+mkdir -p /home/fr/fr_fr/fr_rs1092/roger/examples/plot_scale/boadkh/output/svat_crop_nitrate
+mv "${TMPDIR}"/SVATCROPNITRATE_*.nc /home/fr/fr_fr/fr_rs1092/roger/examples/plot_scale/boadkh/output/svat_crop_nitrate
