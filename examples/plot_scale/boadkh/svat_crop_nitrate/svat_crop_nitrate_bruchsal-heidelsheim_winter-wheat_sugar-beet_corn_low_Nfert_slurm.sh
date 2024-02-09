10dbb70caf67429568ca18bb240e854ed05cd735
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2024-02-09 13:35:34 +0100

    modified examples

---
+++ examples/plot_scale/boadkh/svat_crop_nitrate/svat_crop_nitrate_bruchsal-heidelsheim_winter-wheat_sugar-beet_corn_low_Nfert_slurm.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#SBATCH --time=12:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=4000
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-user=robin.schwemmle@hydrology.uni-freiburg.de
+#SBATCH --job-name=svat_crop_nitrate_bruchsal-heidelsheim_winter-wheat_sugar-beet_corn_low_Nfert
+#SBATCH --output=svat_crop_nitrate_bruchsal-heidelsheim_winter-wheat_sugar-beet_corn_low_Nfert.out
+#SBATCH --error=svat_crop_nitrate_bruchsal-heidelsheim_winter-wheat_sugar-beet_corn_low_Nfert_err.out
+#SBATCH --export=ALL
+ 
+eval "$(conda shell.bash hook)"
+conda activate roger
+cd /home/fr/fr_fr/fr_rs1092/roger/examples/plot_scale/boadkh/svat_crop_nitrate
+ 
+# Copy fluxes and states from global workspace to local SSD
+echo "Copy fluxes and states from global workspace to local SSD"
+# Compares hashes
+checksum_gws=$(shasum -a 256 /home/fr/fr_fr/fr_rs1092/roger/examples/plot_scale/boadkh/output/svat_crop/SVATCROP_bruchsal-heidelsheim_winter-wheat_sugar-beet_corn.nc | cut -f 1 -d " ")
+checksum_ssd=0a
+cp /home/fr/fr_fr/fr_rs1092/roger/examples/plot_scale/boadkh/output/svat_crop/SVATCROP_bruchsal-heidelsheim_winter-wheat_sugar-beet_corn.nc "${TMPDIR}"
+# Wait for termination of moving files
+while [ "${checksum_gws}" != "${checksum_ssd}" ]; do
+sleep 10
+checksum_ssd=$(shasum -a 256 "${TMPDIR}"/SVATCROP_bruchsal-heidelsheim_winter-wheat_sugar-beet_corn.nc | cut -f 1 -d " ")
+done
+echo "Copying was successful"
+ 
+python svat_crop_nitrate.py -b jax -d cpu --float-type float64 --location bruchsal-heidelsheim --crop-rotation-scenario winter-wheat_sugar-beet_corn --fertilization-intensity low -td "${TMPDIR}"
+# Move output from local SSD to global workspace
+echo "Move output to /home/fr/fr_fr/fr_rs1092/roger/examples/plot_scale/boadkh/output/svat_crop_nitrate"
+mkdir -p /home/fr/fr_fr/fr_rs1092/roger/examples/plot_scale/boadkh/output/svat_crop_nitrate
+mv "${TMPDIR}"/SVATCROPNITRATE_*.nc /home/fr/fr_fr/fr_rs1092/roger/examples/plot_scale/boadkh/output/svat_crop_nitrate
---
+++ examples/plot_scale/boadkh/svat_crop_nitrate/svat_crop_nitrate_stockach_summer-wheat_winter-wheat_winter-rape_yellow-mustard_low_Nfert_slurm.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#SBATCH --time=12:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=4000
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-user=robin.schwemmle@hydrology.uni-freiburg.de
+#SBATCH --job-name=svat_crop_nitrate_stockach_summer-wheat_winter-wheat_winter-rape_yellow-mustard_low_Nfert
+#SBATCH --output=svat_crop_nitrate_stockach_summer-wheat_winter-wheat_winter-rape_yellow-mustard_low_Nfert.out
+#SBATCH --error=svat_crop_nitrate_stockach_summer-wheat_winter-wheat_winter-rape_yellow-mustard_low_Nfert_err.out
+#SBATCH --export=ALL
+ 
+eval "$(conda shell.bash hook)"
+conda activate roger
+cd /home/fr/fr_fr/fr_rs1092/roger/examples/plot_scale/boadkh/svat_crop_nitrate
+ 
+# Copy fluxes and states from global workspace to local SSD
+echo "Copy fluxes and states from global workspace to local SSD"
+# Compares hashes
+checksum_gws=$(shasum -a 256 /home/fr/fr_fr/fr_rs1092/roger/examples/plot_scale/boadkh/output/svat_crop/SVATCROP_stockach_summer-wheat_winter-wheat_winter-rape_yellow-mustard.nc | cut -f 1 -d " ")
+checksum_ssd=0a
+cp /home/fr/fr_fr/fr_rs1092/roger/examples/plot_scale/boadkh/output/svat_crop/SVATCROP_stockach_summer-wheat_winter-wheat_winter-rape_yellow-mustard.nc "${TMPDIR}"
+# Wait for termination of moving files
+while [ "${checksum_gws}" != "${checksum_ssd}" ]; do
+sleep 10
+checksum_ssd=$(shasum -a 256 "${TMPDIR}"/SVATCROP_stockach_summer-wheat_winter-wheat_winter-rape_yellow-mustard.nc | cut -f 1 -d " ")
+done
+echo "Copying was successful"
+ 
+python svat_crop_nitrate.py -b jax -d cpu --float-type float64 --location stockach --crop-rotation-scenario summer-wheat_winter-wheat_winter-rape_yellow-mustard --fertilization-intensity low -td "${TMPDIR}"
+# Move output from local SSD to global workspace
+echo "Move output to /home/fr/fr_fr/fr_rs1092/roger/examples/plot_scale/boadkh/output/svat_crop_nitrate"
+mkdir -p /home/fr/fr_fr/fr_rs1092/roger/examples/plot_scale/boadkh/output/svat_crop_nitrate
+mv "${TMPDIR}"/SVATCROPNITRATE_*.nc /home/fr/fr_fr/fr_rs1092/roger/examples/plot_scale/boadkh/output/svat_crop_nitrate
