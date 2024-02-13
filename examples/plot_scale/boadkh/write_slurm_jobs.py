987c363cdce776130adfaf95b07863877333a13e
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2024-02-13 19:54:19 +0100

    modified examples

--- examples/plot_scale/boadkh/write_slurm_jobs.py
+++ examples/plot_scale/boadkh/write_slurm_jobs.py
@@ -23,6 +23,7 @@ def main():
                                "summer-wheat_winter-wheat_silage-corn_yellow-mustard", "summer-wheat_winter-wheat_winter-rape_yellow-mustard",
                                "winter-wheat_silage-corn_yellow-mustard", "winter-wheat_sugar-beet_silage-corn_yellow-mustard",
                                "summer-wheat_winter-wheat_winter-rape_yellow-mustard"]
+    crop_rotation_scenarios = ["winter-wheat_clover_silage-corn"]
     fertilization_intensities = ["low", "medium", "high"]
 
     csv_file = base_path / "clust-id_shp-id_clust-flag.csv"
