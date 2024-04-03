ed6f43cf960381bfebec0279c6af4a8b4edff339
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2024-04-03 15:20:49 +0200

    modified examples

--- examples/plot_scale/rietholzbach/svat_oxygen18_monte_carlo/submit_parallel_jobs.sh
+++ examples/plot_scale/rietholzbach/svat_oxygen18_monte_carlo/submit_parallel_jobs.sh
@@ -2,6 +2,6 @@
 
 cd ~/roger/examples/plot_scale/rietholzbach/svat_oxygen18_monte_carlo
 
-sbatch --partition single svat18O_adp_parallel_slurm.sh
-sbatch --partition single svat18O_adpt_parallel_slurm.sh
+sbatch --partition single svat18O_adp_mc_parallel_slurm.sh
+sbatch --partition single svat18O_adpt_mc_parallel_slurm.sh
 
