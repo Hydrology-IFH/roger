6fe029fb9cf4989032d4efe3b95a48d365c0a219
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2024-04-11 11:22:00 +0200

    modified examples

--- examples/plot_scale/freiburg_altheim_kupferzell/write_slurm_jobs.py
+++ examples/plot_scale/freiburg_altheim_kupferzell/write_slurm_jobs.py
@@ -312,7 +312,7 @@ def main():
                 output_path_ws = base_path_ws / "svat_transport"
                 lines = []
                 lines.append("#!/bin/bash\n")
-                lines.append("#SBATCH --time=16:00:00\n")
+                lines.append("#SBATCH --time=20:00:00\n")
                 lines.append("#SBATCH --nodes=1\n")
                 lines.append("#SBATCH --ntasks=4\n")
                 lines.append("#SBATCH --cpus-per-task=1\n")
