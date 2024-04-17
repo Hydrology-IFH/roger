63c4e9c25e3fa7e926c4f9a02592fb79ec282e36
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2024-04-17 12:16:21 +0200

    modified requirements for doc

--- examples/plot_scale/rietholzbach/svat_oxygen18_sensitivity/submit_parallel_jobs.sh
+++ examples/plot_scale/rietholzbach/svat_oxygen18_sensitivity/submit_parallel_jobs.sh
@@ -2,6 +2,7 @@
 
 cd ~/roger/examples/plot_scale/rietholzbach_new/svat_oxygen18_sensitivity
 
+sbatch --partition single svat18O_pi_sa_parallel_slurm.sh
+sbatch --partition single svat18O_cm_sa_parallel_slurm.sh
 sbatch --partition single svat18O_adp_sa_parallel_slurm.sh
 sbatch --partition single svat18O_adpt_sa_parallel_slurm.sh
-
