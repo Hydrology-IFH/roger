912d7b322eb9c3e52542b8922e0374b9ef4df71d
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2024-05-27 12:40:08 +0200

    updated environments

--- examples/plot_scale/rietholzbach/svat_oxygen18_sensitivity/svat18O_adp_sa_parallel_slurm.sh
+++ examples/plot_scale/rietholzbach/svat_oxygen18_sensitivity/svat18O_adp_sa_parallel_slurm.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --time=48:00:00
+#SBATCH --time=12:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=32
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=180000
+#SBATCH --mem=100000
 #SBATCH --mail-type=FAIL
 #SBATCH --mail-user=robin.schwemmle@hydrology.uni-freiburg.de
 #SBATCH --job-name=svat18O_adp_sa
@@ -14,7 +14,7 @@
 # load module dependencies
 module load devel/cuda/10.2
 module load devel/cudnn/10.2
-module load lib/hdf5/1.12.2-gnu-12.1-openmpi-4.1
+module load lib/hdf5/1.14.4-gnu-13.3-openmpi-5.0
 # prevent memory issues for Open MPI 4.1.x
 export OMPI_MCA_btl="self,smcuda,vader,tcp"
 export OMP_NUM_THREADS=1
@@ -24,6 +24,6 @@ cd /home/fr/fr_fr/fr_rs1092/roger/examples/plot_scale/rietholzbach/svat_oxygen18
  
 mpirun --bind-to core --map-by core -report-bindings python svat_oxygen18.py -b jax -d cpu -n 32 1 --float-type float64 -ns 13312 -tms advection-dispersion-power -td "${TMPDIR}"
 # Move output from local SSD to global workspace
-echo "Move output to /pfs/work7/workspace/scratch/fr_rs1092-workspace/roger/examples/plot_scale/rietholzbach/svat_oxygen18_sensitivity/output"
-mkdir -p /pfs/work7/workspace/scratch/fr_rs1092-workspace/roger/examples/plot_scale/rietholzbach/svat_oxygen18_sensitivity/output
-mv "${TMPDIR}"/SVATTRANSPORT_*.nc /pfs/work7/workspace/scratch/fr_rs1092-workspace/roger/examples/plot_scale/rietholzbach/svat_oxygen18_sensitivity/output
+echo "Move output to /home/fr/fr_fr/fr_rs1092/roger/examples/plot_scale/rietholzbach/svat_oxygen18_sensitivity/output"
+mkdir -p /home/fr/fr_fr/fr_rs1092/roger/examples/plot_scale/rietholzbach/svat_oxygen18_sensitivity/output
+mv "${TMPDIR}"/SVATTRANSPORT_*.nc /home/fr/fr_fr/fr_rs1092/roger/examples/plot_scale/rietholzbach/svat_oxygen18_sensitivity/output
