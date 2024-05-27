f17e087200f85905f327792890f1e9f4bccc5f54
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2024-05-27 14:24:12 +0200

    updated environments

--- install_roger_mpi_on_bwuc2.sh
+++ install_roger_mpi_on_bwuc2.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 conda activate roger-mpi
-module load lib/hdf5/1.14.4-gnu-13.3-openmpi-5.0
+module load lib/hdf5/1.14.4-gnu-13.3-openmpi-4.1
 pip install mpi4py --no-binary mpi4py
 echo {$HDF5_VERSION}
-HDF5_VERSION=1.14.4 CC="mpicc" HDF5_MPI="ON" HDF5_DIR=/cluster/bwhpc/common/lib/hdf5/1.14.4-gnu-13.3-openmpi-5.0 pip install --no-binary=h5py h5py==3.11.0
+HDF5_VERSION=1.14.4 CC="mpicc" HDF5_MPI="ON" HDF5_DIR=/cluster/bwhpc/common/lib/hdf5/1.14.4-gnu-13.3-openmpi-4.1 pip install --no-binary=h5py h5py==3.11.0
 pip install h5netcdf --no-build-isolation
 pip install mpi4jax --no-build-isolation
