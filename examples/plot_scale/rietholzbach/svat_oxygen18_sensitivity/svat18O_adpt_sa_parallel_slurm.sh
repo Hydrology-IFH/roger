7369a7376e89b3ca27631dbb6e3f4de7008a9af1
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2024-06-25 17:15:54 +0200

    modified examples

--- examples/plot_scale/rietholzbach/svat_oxygen18_sensitivity/svat18O_adpt_sa_parallel_slurm.sh
+++ examples/plot_scale/rietholzbach/svat_oxygen18_sensitivity/svat18O_adpt_sa_parallel_slurm.sh
@@ -3,7 +3,6 @@
 #SBATCH --nodes=4
 #SBATCH --ntasks-per-node=8
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=256000
 #SBATCH --mem-per-cpu=8000
 #SBATCH --mail-type=FAIL
 #SBATCH --mail-user=robin.schwemmle@hydrology.uni-freiburg.de
