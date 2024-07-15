4605eed3d70d034f0384c4f8cda8d22733a9423c
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2024-07-15 11:31:46 +0200

    modified examples

--- examples/plot_scale/boadkh/svat_crop_nitrate_sobol_nitrogen/svat_crop_nitrate_5-3_4262994_1_freiburg_winter-wheat_silage-corn_yellow-mustard_medium_Nfert_slurm.sh
+++ examples/plot_scale/boadkh/svat_crop_nitrate_sobol_nitrogen/svat_crop_nitrate_5-3_4262994_1_freiburg_winter-wheat_silage-corn_yellow-mustard_medium_Nfert_slurm.sh
@@ -33,7 +33,7 @@ checksum_ssd=$(shasum -a 256 "${TMPDIR}"/SVATCROP_freiburg_winter-wheat_silage-c
 done
 echo "Copying was successful"
  
-python svat_crop_nitrate.py -b jax -d cpu --float-type float64 --row 36 --id 5-3_4262994_1 --location freiburg --crop-rotation-scenario winter-wheat_silage-corn_yellow-mustard --fertilization-intensity medium -td "${TMPDIR}"
+python svat_crop_nitrate.py -b jax -d cpu -n 32 1 --float-type float64 --row 36 --id 5-3_4262994_1 --location freiburg --crop-rotation-scenario winter-wheat_silage-corn_yellow-mustard --fertilization-intensity medium -td "${TMPDIR}"
 # Move output from local SSD to global workspace
 echo "Move output to /pfs/work7/workspace/scratch/fr_rs1092-workspace1/roger/examples/plot_scale/boadkh/svat_crop_nitrate_sobol_nitrogen"
 mkdir -p /pfs/work7/workspace/scratch/fr_rs1092-workspace1/roger/examples/plot_scale/boadkh/svat_crop_nitrate_sobol_nitrogen
