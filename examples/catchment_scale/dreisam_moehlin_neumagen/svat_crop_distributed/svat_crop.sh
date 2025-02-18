2058867118580e67112db0418fa34155c19410ad
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2025-02-18 17:02:13 +0100

    modified examples

--- examples/catchment_scale/dreisam_moehlin_neumagen/svat_crop_distributed/svat_crop.sh
+++ examples/catchment_scale/dreisam_moehlin_neumagen/svat_crop_distributed/svat_crop.sh
@@ -6,17 +6,17 @@
 #SBATCH --mem=24000
 #SBATCH --mail-type=FAIL
 #SBATCH --mail-user=robin.schwemmle@hydrology.uni-freiburg.de
-#SBATCH --job-name=svat
-#SBATCH --output=svat.out
-#SBATCH --error=svat_err.out
+#SBATCH --job-name=svat_crop
+#SBATCH --output=svat_crop.out
+#SBATCH --error=svat_crop_err.out
 #SBATCH --export=ALL
  
 eval "$(conda shell.bash hook)"
 conda activate roger
-cd /home/fr/fr_fr/fr_rs1092/roger/examples/catchment_scale/dreisam_moehlin_neumagen/svat_distributed
+cd /home/fr/fr_fr/fr_rs1092/roger/examples/catchment_scale/dreisam_moehlin_neumagen/svat_crop_distributed
   
 python svat_crop.py -b jax -td "${TMPDIR}"
 # Move output from local SSD to global workspace
-echo "Move output to /home/fr/fr_fr/fr_rs1092/roger/examples/catchment_scale/dreisam_moehlin_neumagen/svat_distributed/output"
-mkdir -p /home/fr/fr_fr/fr_rs1092/roger/examples/catchment_scale/dreisam_moehlin_neumagen/svat_distributed/output
-mv "${TMPDIR}"/SVAT.*.nc /home/fr/fr_fr/fr_rs1092/roger/examples/catchment_scale/dreisam_moehlin_neumagen/svat_distributed/output
+echo "Move output to /home/fr/fr_fr/fr_rs1092/roger/examples/catchment_scale/dreisam_moehlin_neumagen/svat_crop_distributed/output"
+mkdir -p /home/fr/fr_fr/fr_rs1092/roger/examples/catchment_scale/dreisam_moehlin_neumagen/svat_crop_distributed/output
+mv "${TMPDIR}"/SVAT.*.nc /home/fr/fr_fr/fr_rs1092/roger/examples/catchment_scale/dreisam_moehlin_neumagen/svat_crop_distributed/output
