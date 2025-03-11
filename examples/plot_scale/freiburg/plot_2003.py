1a3b43c2cb60805ae0f29637bbbe6fd3b30c189a
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2025-03-11 18:41:00 +0100

    modified examples

--- examples/plot_scale/freiburg/plot_2003.py
+++ examples/plot_scale/freiburg/plot_2003.py
@@ -68,40 +68,101 @@ def main():
 
     for irrigation_scenario in irrigation_scenarios:
         for crop_rotation_scenario in crop_rotation_scenarios:
-            fig, axs = plt.subplots(3, 1, figsize=(6, 6), sharey=True)
-            for i, soil_type in enumerate(soil_types):
-                data = dict_data["no_irrigation"][irrigation_scenario][crop_rotation_scenario][soil_type].loc["2003-04-01":"2003-09-30" , :]
-                axs[i].plot(data.index, data["canopy_cover"], color="red")
-                axs[i].set_ylim(0, )
-            axs[1].set_ylabel("Bodenbedeckung [-]")
-            axs[-1].set_xlabel("[Jahr-Monat]")
+            # fig, axs = plt.subplots(3, 1, figsize=(6, 6), sharey=True)
+            # for i, soil_type in enumerate(soil_types):
+            #     data = dict_data["no_irrigation"][irrigation_scenario][crop_rotation_scenario][soil_type].loc["2003-04-01":"2003-09-30" , :]
+            #     axs[i].plot(data.index, data["canopy_cover"], color="red")
+            #     axs[i].set_ylim(0, )
+            # axs[1].set_ylabel("Bodenbedeckung [-]")
+            # axs[-1].set_xlabel("[Jahr-Monat]")
+            # fig.tight_layout()
+            # file = base_path / "figures" / f"canopy_cover_no_irrigation_{irrigation_scenario}_irr_demand_{crop_rotation_scenario}.png"
+            # fig.savefig(file, dpi=300)
+
+            # for i, soil_type in enumerate(soil_types):
+            #     data = dict_data["irrigation"][irrigation_scenario][crop_rotation_scenario][soil_type].loc["2003-04-01":"2003-09-30" , :]
+            #     axs[i].plot(data.index, data["canopy_cover"], color="red")
+            #     axs[i].set_ylim(0, )
+            # axs[1].set_ylabel("Bodenbedeckung [-]")
+            # axs[-1].set_xlabel("[Jahr-Monat]")
+            # fig.tight_layout()
+            # file = base_path / "figures" / f"canopy_cover_irrigation_{irrigation_scenario}_irr_demand_{crop_rotation_scenario}.png"
+            # fig.savefig(file, dpi=300)
+
+            # fig, axs = plt.subplots(3, 1, figsize=(6, 6), sharey=True)
+            # for i, soil_type in enumerate(soil_types):
+            #     data = dict_data["no_irrigation"][irrigation_scenario][crop_rotation_scenario][soil_type].loc["2003-04-01":"2003-09-30" , :]
+            #     axs[i].plot(data.index, data["canopy_cover"], color="red")
+            #     data = dict_data["irrigation"][irrigation_scenario][crop_rotation_scenario][soil_type].loc["2003-04-01":"2003-09-30" , :]
+            #     axs[i].plot(data.index, data["canopy_cover"], color="blue")
+            #     axs[i].set_ylim(0, )
+            # axs[1].set_ylabel("Bodenbedeckung [-]")
+            # axs[-1].set_xlabel("[Jahr-Monat]")
+            # fig.tight_layout()
+            # file = base_path / "figures" / f"canopy_cover_no_irrigation_{irrigation_scenario}_irr_demand_{crop_rotation_scenario}.png"
+            # fig.savefig(file, dpi=300)
+
+            fig, axs = plt.subplots(1, 1, figsize=(6, 2.5))
+            soil_type = "silty_soil"
+            data = dict_data["no_irrigation"][irrigation_scenario][crop_rotation_scenario][soil_type].loc["2003-03-01":"2003-07-30" , :]
+            axs.plot(data.index, data["canopy_cover"] * 0.75, color="red")
+            data = dict_data["irrigation"][irrigation_scenario][crop_rotation_scenario][soil_type].loc["2003-04-01":"2003-07-30" , :]
+            axs.plot(data.index, data["canopy_cover"], color="blue")
+            axs.set_ylim(0, )
+            axs.set_xlim(data.index[0], data.index[-1])
+            axs.set_ylabel("Bodenbedeckung [-]")
+            axs.set_xlabel("[Jahr-Monat]")
+            # rotate ticklabels of x-axis
+            axs.set_xticklabels(axs.get_xticklabels(), rotation=20)
             fig.tight_layout()
-            file = base_path / "figures" / f"canopy_cover_no_irrigation_{irrigation_scenario}_irr_demand_{crop_rotation_scenario}.png"
+            file = base_path / "figures" / f"canopy_cover_{irrigation_scenario}_irr_demand_{crop_rotation_scenario}_silty_soil.png"
             fig.savefig(file, dpi=300)
 
-            for i, soil_type in enumerate(soil_types):
-                data = dict_data["irrigation"][irrigation_scenario][crop_rotation_scenario][soil_type].loc["2003-04-01":"2003-09-30" , :]
-                axs[i].plot(data.index, data["canopy_cover"], color="red")
-                axs[i].set_ylim(0, )
-            axs[1].set_ylabel("Bodenbedeckung [-]")
-            axs[-1].set_xlabel("[Jahr-Monat]")
+            fig, axs = plt.subplots(1, 1, figsize=(6, 2.5))
+            soil_type = "silty_soil"
+            data = dict_data["no_irrigation"][irrigation_scenario][crop_rotation_scenario][soil_type].loc["2003-03-01":"2003-07-30" , :]
+            axs.plot(data.index, (data["z_root"]/1000) * 0.75, color="red")
+            data = dict_data["irrigation"][irrigation_scenario][crop_rotation_scenario][soil_type].loc["2003-04-01":"2003-07-30" , :]
+            axs.plot(data.index, data["z_root"]/1000, color="blue")
+            axs.set_ylim(0, )
+            # reverse y-axis
+            axs.invert_yaxis()
+            axs.set_xlim(data.index[0], data.index[-1])
+            axs.set_ylabel("Wurzeltiefe [m]")
+            axs.set_xlabel("[Jahr-Monat]")
+            # rotate ticklabels of x-axis
+            axs.set_xticklabels(axs.get_xticklabels(), rotation=20)
             fig.tight_layout()
-            file = base_path / "figures" / f"canopy_cover_irrigation_{irrigation_scenario}_irr_demand_{crop_rotation_scenario}.png"
+            file = base_path / "figures" / f"root_depth_{irrigation_scenario}_irr_demand_{crop_rotation_scenario}_silty_soil.png"
             fig.savefig(file, dpi=300)
 
-            fig, axs = plt.subplots(3, 1, figsize=(6, 6), sharey=True)
-            for i, soil_type in enumerate(soil_types):
-                data = dict_data["no_irrigation"][irrigation_scenario][crop_rotation_scenario][soil_type].loc["2003-04-01":"2003-09-30" , :]
-                axs[i].plot(data.index, data["canopy_cover"], color="red")
-                data = dict_data["irrigation"][irrigation_scenario][crop_rotation_scenario][soil_type].loc["2003-04-01":"2003-09-30" , :]
-                axs[i].plot(data.index, data["canopy_cover"], color="blue")
-                axs[i].set_ylim(0, )
-            axs[1].set_ylabel("Bodenbedeckung [-]")
-            axs[-1].set_xlabel("[Jahr-Monat]")
+            fig, axs = plt.subplots(1, 1, figsize=(6, 2.5))
+            soil_type = "silty_soil"
+            data = dict_data["no_irrigation"][irrigation_scenario][crop_rotation_scenario][soil_type].loc["2003-03-01":"2003-07-30" , :]
+            axs.plot(data.index, data["irrigation_demand"], color="red")
+            axs.set_ylim(0, )
+            axs.set_xlim(data.index[0], data.index[-1])
+            axs.set_ylabel("potentieller\n Bewaesserungsbedarf\n [mm/Tag]")
+            axs.set_xlabel("[Jahr-Monat]")
+            # rotate ticklabels of x-axis
+            axs.set_xticklabels(axs.get_xticklabels(), rotation=20)
             fig.tight_layout()
-            file = base_path / "figures" / f"canopy_cover_no_irrigation_{irrigation_scenario}_irr_demand_{crop_rotation_scenario}.png"
+            file = base_path / "figures" / f"{irrigation_scenario}_irr_demand_{crop_rotation_scenario}_silty_soil.png"
             fig.savefig(file, dpi=300)
 
+            fig, axs = plt.subplots(1, 1, figsize=(6, 2.5))
+            soil_type = "silty_soil"
+            data = dict_data["irrigation"][irrigation_scenario][crop_rotation_scenario][soil_type].loc["2003-03-01":"2003-07-30" , :]
+            axs.plot(data.index, data["irrig"], color="blue")
+            axs.set_ylim(0, )
+            axs.set_xlim(data.index[0], data.index[-1])
+            axs.set_ylabel("Bewaesserung\n [mm/Tag]")
+            axs.set_xlabel("[Jahr-Monat]")
+            # rotate ticklabels of x-axis
+            axs.set_xticklabels(axs.get_xticklabels(), rotation=20)
+            fig.tight_layout()
+            file = base_path / "figures" / f"{irrigation_scenario}_irrig_{crop_rotation_scenario}_silty_soil.png"
+            fig.savefig(file, dpi=300)
 
     return
 
