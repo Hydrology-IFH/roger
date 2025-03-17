cdc6932193651cfe9cfdb2ef5c3c0999a17e168f
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2025-03-17 16:32:07 +0100

    modified examples

--- examples/plot_scale/freiburg/irrigation/run_roger_parallel.sh
+++ examples/plot_scale/freiburg/irrigation/run_roger_parallel.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 eval "$(conda shell.bash hook)"
 conda activate roger
-./run_roger_30-ufc.sh &
-./run_roger_45-ufc.sh &
-./run_roger_50-ufc.sh &
-./run_roger_80-ufc.sh &
-./run_roger_crop-specific.sh &
+nohup ./run_roger_35-ufc.sh &
+nohup ./run_roger_45-ufc.sh &
+nohup ./run_roger_50-ufc.sh &
+nohup ./run_roger_80-ufc.sh &
+nohup ./run_roger_crop-specific.sh &
 
 python merge_output.py
 python write_simulations_to_csv.py
