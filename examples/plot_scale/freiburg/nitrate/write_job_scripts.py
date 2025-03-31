bb17d518cfe711883304ddbdda873559191a6dc3
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2025-03-31 15:49:16 +0200

    modified examples

--- examples/plot_scale/freiburg/nitrate/write_job_scripts.py
+++ examples/plot_scale/freiburg/nitrate/write_job_scripts.py
@@ -25,7 +25,24 @@ def main():
     for irrigation_scenario in irrigation_scenarios:
         for crop_rotation_scenario in crop_rotation_scenarios:
             lines.append(
-                'python svat_crop_nitrate.py -b numpy -d cpu --irrigation-scenario %s --crop-rotation-scenario %s\n' % (irrigation_scenario, crop_rotation_scenario)
+                'python svat_crop_nitrate.py -b numpy -d cpu --soil-compaction-scenario no_compaction --irrigation-scenario %s --crop-rotation-scenario %s\n' % (irrigation_scenario, crop_rotation_scenario)
+            )
+    lines.append('python merge_output.py\n')
+    lines.append('python write_simulations_to_csv.py')
+    file = open(file_path, "w")
+    file.writelines(lines)
+    file.close()
+    subprocess.Popen(f"chmod +x {file_path}", shell=True)
+
+    file_path = base_path / "run_roger_compaction.sh"
+    lines = []
+    lines.append('#!/bin/bash\n')
+    lines.append('eval "$(conda shell.bash hook)"\n')
+    lines.append("conda activate roger\n")
+    for irrigation_scenario in irrigation_scenarios:
+        for crop_rotation_scenario in crop_rotation_scenarios:
+            lines.append(
+                'python svat_crop_nitrate.py -b numpy -d cpu --soil-compaction-scenario compaction --irrigation-scenario %s --crop-rotation-scenario %s\n' % (irrigation_scenario, crop_rotation_scenario)
             )
     lines.append('python merge_output.py\n')
     lines.append('python write_simulations_to_csv.py')
@@ -39,9 +56,22 @@ def main():
         lines.append('#!/bin/bash\n')
         for crop_rotation_scenario in crop_rotation_scenarios:
             lines.append(
-                'python svat_crop_nitrate.py -b numpy -d cpu --irrigation-scenario %s --crop-rotation-scenario %s\n' % (irrigation_scenario, crop_rotation_scenario)
+                'python svat_crop_nitrate.py -b numpy -d cpu --soil-compaction-scenario no_compaction --irrigation-scenario %s --crop-rotation-scenario %s\n' % (irrigation_scenario, crop_rotation_scenario)
             )
         file_path = base_path / f"run_roger_{irrigation_scenario}.sh"
+        file = open(file_path, "w")
+        file.writelines(lines)
+        file.close()
+        subprocess.Popen(f"chmod +x {file_path}", shell=True)
+
+    for irrigation_scenario in irrigation_scenarios:
+        lines = []
+        lines.append('#!/bin/bash\n')
+        for crop_rotation_scenario in crop_rotation_scenarios:
+            lines.append(
+                'python svat_crop_nitrate.py -b numpy -d cpu --soil-compaction-scenario compaction --irrigation-scenario %s --crop-rotation-scenario %s\n' % (irrigation_scenario, crop_rotation_scenario)
+            )
+        file_path = base_path / f"run_roger_{irrigation_scenario}_compaction.sh"
         file = open(file_path, "w")
         file.writelines(lines)
         file.close()
