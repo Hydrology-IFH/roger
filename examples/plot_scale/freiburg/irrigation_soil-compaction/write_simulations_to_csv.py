fe51f8fb504736751bc77a5366d8d177474c7621
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2025-03-31 16:52:01 +0200

    modified examples

--- examples/plot_scale/freiburg/irrigation_soil-compaction/write_simulations_to_csv.py
+++ examples/plot_scale/freiburg/irrigation_soil-compaction/write_simulations_to_csv.py
@@ -34,90 +34,84 @@ def main():
         config = yaml.safe_load(file)
 
     # identifiers of the simulations
-    irrigation_demand_scenarios = config["irrigation_scenarios"]
+    irrigation_scenarios = config["irrigation_scenarios"]
     crop_rotation_scenarios = config["crop_rotation_scenarios"]
     soil_types = df_parameters.index.to_list()
-    for irrigation_demand_scenario in irrigation_demand_scenarios:
-        if irrigation_demand_scenario == "20-ufc":
+    for irrigation_scenario in irrigation_scenarios:
+        if irrigation_scenario == "20-ufc":
             c_irr = 0.2
-        elif irrigation_demand_scenario == "30-ufc":
+        elif irrigation_scenario == "30-ufc":
             c_irr = 0.30
-        elif irrigation_demand_scenario == "50-ufc":
+        elif irrigation_scenario == "50-ufc":
             c_irr = 0.50
-        if not os.path.exists(str(base_path / "output" / dir_name / irrigation_demand_scenario)):
-            os.makedirs(base_path / "output" / dir_name / irrigation_demand_scenario)
-        for crop_rotation_scenario in crop_rotation_scenarios:
-            roger_file = (
-                base_path
-                / "output" 
-                / dir_name 
-                / f"SVATCROP_{crop_rotation_scenario}.nc"
-            )
-            if os.path.exists(roger_file):
-                ds = xr.open_dataset(roger_file, engine="h5netcdf")
-                # assign date
-                days = ds["Time"].values / onp.timedelta64(24 * 60 * 60, "s")
-                date = num2date(
-                    days,
-                    units=f"days since {ds['Time'].attrs['time_origin']}",
-                    calendar="standard",
-                    only_use_cftime_datetimes=False,
+        if os.path.exists(str(base_path / "output" / dir_name / irrigation_scenario)):
+            for crop_rotation_scenario in crop_rotation_scenarios:
+                roger_file = (
+                    base_path
+                    / "output" 
+                    / dir_name 
+                    / irrigation_scenario
+                    / f"SVATCROP_{crop_rotation_scenario}.nc"
                 )
-                ds = ds.assign_coords(Time=("Time", date))
-                dir_csv_files = base_path / "output" / dir_name / irrigation_demand_scenario / crop_rotation_scenario
-                if not os.path.exists(dir_csv_files):
-                    os.makedirs(dir_csv_files)
-                for x, soil_type in enumerate(soil_types):
-                    dir_csv_files = base_path / "output" / dir_name / irrigation_demand_scenario / crop_rotation_scenario / soil_type
+                if os.path.exists(roger_file):
+                    ds = xr.open_dataset(roger_file, engine="h5netcdf")
+                    # assign date
+                    days = ds["Time"].values / onp.timedelta64(24 * 60 * 60, "s")
+                    date = num2date(
+                        days,
+                        units=f"days since {ds['Time'].attrs['time_origin']}",
+                        calendar="standard",
+                        only_use_cftime_datetimes=False,
+                    )
+                    ds = ds.assign_coords(Time=("Time", date))
+                    dir_csv_files = base_path / "output" / dir_name / irrigation_scenario / crop_rotation_scenario
                     if not os.path.exists(dir_csv_files):
                         os.makedirs(dir_csv_files)
-                    # write simulation to csv
-                    df_simulation = pd.DataFrame(
-                        index=date, columns=["precip", "canopy_cover", "z_root", "theta_fc", "theta_irrig", "theta_rz", "irrigation_demand", "root_ventilation", "ta_max", "heat_stress", "transp", "perc", "lu_id", "crop_type"]
-                    )
-                    df_simulation.loc[:, "precip"] = ds["prec"].isel(x=x, y=0).values
-                    df_simulation.loc[:, "canopy_cover"] = ds["ground_cover"].isel(x=x, y=0).values
-                    df_simulation.loc[:, "z_root"] = ds["z_root"].isel(x=x, y=0).values
-                    df_simulation.loc[:, "theta_rz"] = ds["theta_rz"].isel(x=x, y=0).values
-                    df_simulation.loc[:, "theta_fc"] = df_parameters.loc[f"{soil_type}", "theta_pwp"] + df_parameters.loc[f"{soil_type}", "theta_ufc"]
-                    df_simulation.loc[:, "theta_irrig"] = df_parameters.loc[f"{soil_type}", "theta_pwp"] + df_parameters.loc[f"{soil_type}", "theta_ufc"]
-                    df_simulation.loc[:, "transp"] = ds["transp"].isel(x=x, y=0).values
-                    df_simulation.loc[:, "perc"] = ds["q_ss"].isel(x=x, y=0).values
-                    df_simulation.loc[:, "ta_max"] = ds["ta_max"].isel(x=x, y=0).values
-                    # calculate heat stress of crops
-                    df_simulation.loc[:, "heat_stress"] = 0
-                    ta_heat_stress_list = [dict_crop_heat_stress[lu_id] for lu_id in ds["lu_id"].isel(x=x, y=0).values]
-                    ta_heat_stress = onp.array(ta_heat_stress_list)
-                    cond = (df_simulation.loc[:, "ta_max"].values >= ta_heat_stress)
-                    df_simulation.loc[cond, "heat_stress"] = 1
-                    df_simulation.loc[:, "lu_id"] = ds["lu_id"].isel(x=x, y=0).values
-                    df_simulation.loc[:, "crop_type"] = [dict_crop_types[lu_id] for lu_id in ds["lu_id"].isel(x=x, y=0).values]
-                    # calculate root ventilation
-                    root_ventilation = ds["theta_rz"].isel(x=x, y=0).values - (df_parameters.loc[f"{soil_type}", "theta_pwp"] + df_parameters.loc[f"{soil_type}", "theta_ufc"])
-                    root_ventilation[root_ventilation < 0] = 0
-                    root_ventilation[root_ventilation > 1] = 1
-                    root_ventilation = (1 - root_ventilation) * 100
-                    df_simulation.loc[:, "root_ventilation"] = root_ventilation
-                    if irrigation_demand_scenario in ["15-ufc", "30-ufc", "50-ufc"]:
-                        theta_irr = df_parameters.loc[f"{soil_type}", "theta_pwp"] + (c_irr * df_parameters.loc[f"{soil_type}", "theta_ufc"])
-                        df_simulation.loc[:, "theta_irrig"] = theta_irr
-                        irr_demand = (theta_irr - ds["theta_rz"].isel(x=x, y=0).values) * ds["z_root"].isel(x=x, y=0).values
-                        irr_demand[irr_demand < 0] = 0
-                        df_simulation.loc[:, "irrigation_demand"] = irr_demand
-                    else:
-                        c_irr_list = [dict_crop_water_stress[lu_id] for lu_id in ds["lu_id"].isel(x=x, y=0).values]
-                        c_irr = onp.array(c_irr_list)
-                        theta_irr = df_parameters.loc[f"{soil_type}", "theta_pwp"] + (c_irr * df_parameters.loc[f"{soil_type}", "theta_ufc"])
-                        df_simulation.loc[:, "theta_irrig"] = theta_irr
-                        irr_demand = (theta_irr - ds["theta_rz"].isel(x=x, y=0).values) * ds["z_root"].isel(x=x, y=0).values
-                        irr_demand[irr_demand < 0] = 0
-                        df_simulation.loc[:, "irrigation_demand"] = irr_demand
-                    df_simulation.columns =[["[mm/day]", "[-]", "[mm]", "[-]", "[-]", "[-]", "[mm]", "[%]", "[degC]", "[day]", "[mm/day]", "[mm/day]", "", ""],
-                                            ["precip", "canopy_cover", "z_root", "theta_fc", "theta_irrig", "theta_rz", "irrigation_demand", "root_ventilation", "ta_max", "heat_stress", "transp", "perc", "lu_id", "crop_type"]]
-                    df_simulation = df_simulation.iloc[1:, :] # remove initial values
-                    df_simulation.to_csv(
-                        dir_csv_files / "simulation.csv", sep=";"
-                    )
+                    for x, soil_type in enumerate(soil_types):
+                        dir_csv_files = base_path / "output" / dir_name / irrigation_scenario / crop_rotation_scenario / soil_type
+                        if not os.path.exists(dir_csv_files):
+                            os.makedirs(dir_csv_files)
+                        # write simulation to csv
+                        df_simulation = pd.DataFrame(
+                            index=date, columns=["precip", "canopy_cover", "z_root", "theta_fc", "theta_irrig", "theta_rz", "irrig", "irrigation_demand", "root_ventilation", "ta_max", "heat_stress", "transp", "perc", "lu_id", "crop_type"]
+                        )
+                        df_simulation.loc[:, "precip"] = onp.where(ds["irrig"].isel(x=x, y=0).values > 0, 0, ds["prec"].isel(x=x, y=0).values)
+                        df_simulation.loc[:, "irrig"] = ds["irrig"].isel(x=x, y=0).values
+                        df_simulation.loc[:, "canopy_cover"] = ds["ground_cover"].isel(x=x, y=0).values
+                        df_simulation.loc[:, "z_root"] = ds["z_root"].isel(x=x, y=0).values
+                        df_simulation.loc[:, "irrigation_demand"] = ds["irr_demand"].isel(x=x, y=0).values
+                        df_simulation.loc[:, "theta_rz"] = ds["theta_rz"].isel(x=x, y=0).values
+                        df_simulation.loc[:, "theta_fc"] = df_parameters.loc[f"{soil_type}", "theta_pwp"] + df_parameters.loc[f"{soil_type}", "theta_ufc"]
+                        df_simulation.loc[:, "transp"] = ds["transp"].isel(x=x, y=0).values
+                        df_simulation.loc[:, "perc"] = ds["q_ss"].isel(x=x, y=0).values
+                        df_simulation.loc[:, "ta_max"] = ds["ta_max"].isel(x=x, y=0).values
+                        # calculate heat stress of crops
+                        df_simulation.loc[:, "heat_stress"] = 0
+                        ta_heat_stress_list = [dict_crop_heat_stress[lu_id] for lu_id in ds["lu_id"].isel(x=x, y=0).values]
+                        ta_heat_stress = onp.array(ta_heat_stress_list)
+                        cond = (df_simulation.loc[:, "ta_max"].values >= ta_heat_stress)
+                        df_simulation.loc[cond, "heat_stress"] = 1
+                        df_simulation.loc[:, "lu_id"] = ds["lu_id"].isel(x=x, y=0).values
+                        df_simulation.loc[:, "crop_type"] = [dict_crop_types[lu_id] for lu_id in ds["lu_id"].isel(x=x, y=0).values]
+                        root_ventilation = ds["theta_rz"].isel(x=x, y=0).values - (df_parameters.loc[f"{soil_type}", "theta_pwp"] + df_parameters.loc[f"{soil_type}", "theta_ufc"])
+                        root_ventilation[root_ventilation < 0] = 0
+                        root_ventilation[root_ventilation > 1] = 1
+                        root_ventilation = (1 - root_ventilation) * 100
+                        df_simulation.loc[:, "root_ventilation"] = root_ventilation
+                        if irrigation_scenario in ["35-ufc", "45-ufc", "50-ufc", "80-ufc"]:
+                            theta_irr = df_parameters.loc[f"{soil_type}", "theta_pwp"] + (c_irr * df_parameters.loc[f"{soil_type}", "theta_ufc"])
+                            df_simulation.loc[:, "theta_irrig"] = theta_irr
+                        else:
+                            c_irr_list = [dict_crop_water_stress[lu_id] for lu_id in ds["lu_id"].isel(x=x, y=0).values]
+                            c_irr = onp.array(c_irr_list)
+                            theta_irr = df_parameters.loc[f"{soil_type}", "theta_pwp"] + (c_irr * df_parameters.loc[f"{soil_type}", "theta_ufc"])
+                            df_simulation.loc[:, "theta_irrig"] = theta_irr
+                        df_simulation.columns =[["[mm/day]", "[-]", "[mm]", "[-]", "[-]", "[-]", "[mm/day]", "[mm]", "[%]", "[degC]", "[day]", "[mm/day]", "[mm/day]", "", ""],
+                                                ["precip", "canopy_cover", "z_root", "theta_fc", "theta_irrig", "theta_rz", "irrig", "irrigation_demand", "root_ventilation", "ta_max", "heat_stress", "transp", "perc", "lu_id", "crop_type"]]
+                        df_simulation = df_simulation.iloc[1:, :] # remove initial values
+                        df_simulation.to_csv(
+                            dir_csv_files / "simulation.csv", sep=";"
+                        )
     return
 
 
