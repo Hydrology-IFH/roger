cf512b66993ee48c5ec053439be74fa851a67ac2
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2025-03-31 12:27:37 +0200

    modified examples

--- roger/core/crop.py
+++ roger/core/crop.py
@@ -1325,6 +1325,9 @@ def update_lu_id(state):
         vs.lu_id, at[2:-2, 2:-2], npx.where(mask3[2:-2, 2:-2], vs.crop_type[2:-2, 2:-2, 2], vs.lu_id[2:-2, 2:-2])
     )
 
+    print("crop_type", vs.crop_type[2, 2, 1])
+    print("lu_id", vs.lu_id[2, 2])
+
     return KernelOutput(lu_id=vs.lu_id)
 
 @roger_kernel
