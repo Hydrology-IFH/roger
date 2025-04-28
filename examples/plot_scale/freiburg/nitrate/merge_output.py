1298fba0b8138f40114bedf18efb3c1e1beb71a6
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2025-04-28 16:35:27 +0200

    modified examples

--- examples/plot_scale/freiburg/nitrate/merge_output.py
+++ examples/plot_scale/freiburg/nitrate/merge_output.py
@@ -77,8 +77,9 @@ def main():
                                         v[:] = time
                                     for var_sim in list(df.variables.keys()):
                                         var_obj = df.variables.get(var_sim)
+                                        print(var_sim)
+                                        print(var_obj.shape)
                                         if var_sim not in list(f.dimensions.keys()) and var_obj.ndim == 3 and var_obj.shape[0] > 2:
-                                            print(var_sim)
                                             v = f.create_variable(
                                                 var_sim, ("x", "y", "Time"), float, compression="gzip", compression_opts=1
                                             )
@@ -88,13 +89,10 @@ def main():
                                         elif (
                                             var_sim not in list(f.dimensions.keys()) and var_obj.ndim == 3 and var_obj.shape[0] <= 2
                                         ):
-                                            print(var_sim)
                                             v = f.create_variable(
                                                 var_sim, ("x", "y"), float, compression="gzip", compression_opts=1
                                             )
                                             vals = onp.array(var_obj, dtype=float)
-                                            print(var_sim)
-                                            print(vals.shape)
                                             v[:, :] = vals.swapaxes(0, 2)[:, :, 0]
                                             v.attrs.update(long_name=var_obj.attrs["long_name"], units=var_obj.attrs["units"])
                             # add year and day of year for nitrate transport model
