567f72143eddeedca6c28e340fb50c43d4bd635d
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2025-06-05 09:41:59 +0200

    modified examples

--- examples/plot_scale/freiburg/nitrate/run_roger-sas-nitrate_crop-specific_compaction.sh
+++ examples/plot_scale/freiburg/nitrate/run_roger-sas-nitrate_crop-specific_compaction.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
-python svat_crop_nitrate.py -b jax -d cpu --soil-compaction-scenario compaction --irrigation-scenario crop-specific --crop-rotation-scenario sugar-beet
-python svat_crop_nitrate.py -b jax -d cpu --soil-compaction-scenario compaction --irrigation-scenario crop-specific --crop-rotation-scenario sugar-beet_yellow-mustard
-python svat_crop_nitrate.py -b jax -d cpu --soil-compaction-scenario compaction --irrigation-scenario crop-specific --crop-rotation-scenario vegetables
-python svat_crop_nitrate.py -b jax -d cpu --soil-compaction-scenario compaction --irrigation-scenario crop-specific --crop-rotation-scenario strawberry
-python svat_crop_nitrate.py -b jax -d cpu --soil-compaction-scenario compaction --irrigation-scenario crop-specific --crop-rotation-scenario winter-wheat_clover
-python svat_crop_nitrate.py -b jax -d cpu --soil-compaction-scenario compaction --irrigation-scenario crop-specific --crop-rotation-scenario summer-wheat_winter-wheat
-python svat_crop_nitrate.py -b jax -d cpu --soil-compaction-scenario compaction --irrigation-scenario crop-specific --crop-rotation-scenario summer-wheat_clover_winter-wheat
+python svat_crop_nitrate.py -b jax -d cpu --soil-compaction-scenario compaction --irrigation-scenario crop-specific --crop-rotation-scenario winter-wheat_clover_silage-corn
+python svat_crop_nitrate.py -b jax -d cpu --soil-compaction-scenario compaction --irrigation-scenario crop-specific --crop-rotation-scenario winter-wheat_sugar-beet_silage-corn
+python svat_crop_nitrate.py -b jax -d cpu --soil-compaction-scenario compaction --irrigation-scenario crop-specific --crop-rotation-scenario summer-wheat_winter-wheat_silage-corn
+python svat_crop_nitrate.py -b jax -d cpu --soil-compaction-scenario compaction --irrigation-scenario crop-specific --crop-rotation-scenario summer-wheat_winter-wheat_winter-rape
+python svat_crop_nitrate.py -b jax -d cpu --soil-compaction-scenario compaction --irrigation-scenario crop-specific --crop-rotation-scenario sugar-beet_winter-wheat_winter-barley
+python svat_crop_nitrate.py -b jax -d cpu --soil-compaction-scenario compaction --irrigation-scenario crop-specific --crop-rotation-scenario grain-corn_winter-wheat_winter-barley
+python svat_crop_nitrate.py -b jax -d cpu --soil-compaction-scenario compaction --irrigation-scenario crop-specific --crop-rotation-scenario grain-corn_winter-wheat_clover
+python svat_crop_nitrate.py -b jax -d cpu --soil-compaction-scenario compaction --irrigation-scenario crop-specific --crop-rotation-scenario summer-wheat_winter-wheat_yellow-mustard
+python svat_crop_nitrate.py -b jax -d cpu --soil-compaction-scenario compaction --irrigation-scenario crop-specific --crop-rotation-scenario winter-wheat_sugar-beet_silage-corn_yellow-mustard
+python svat_crop_nitrate.py -b jax -d cpu --soil-compaction-scenario compaction --irrigation-scenario crop-specific --crop-rotation-scenario summer-wheat_winter-wheat_silage-corn_yellow-mustard
+python svat_crop_nitrate.py -b jax -d cpu --soil-compaction-scenario compaction --irrigation-scenario crop-specific --crop-rotation-scenario summer-wheat_winter-wheat_winter-rape_yellow-mustard
+python svat_crop_nitrate.py -b jax -d cpu --soil-compaction-scenario compaction --irrigation-scenario crop-specific --crop-rotation-scenario sugar-beet_winter-wheat_winter-barley_yellow-mustard
+python svat_crop_nitrate.py -b jax -d cpu --soil-compaction-scenario compaction --irrigation-scenario crop-specific --crop-rotation-scenario grain-corn_winter-wheat_winter-barley_yellow-mustard
+python svat_crop_nitrate.py -b jax -d cpu --soil-compaction-scenario compaction --irrigation-scenario crop-specific --crop-rotation-scenario asparagus
