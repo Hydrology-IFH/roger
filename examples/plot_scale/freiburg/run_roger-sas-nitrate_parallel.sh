4260092f3fdd4291c30980c8926da4ae2a1d0c22
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2025-06-05 10:09:47 +0200

    modified examples

--- examples/plot_scale/freiburg/run_roger-sas-nitrate_parallel.sh
+++ examples/plot_scale/freiburg/run_roger-sas-nitrate_parallel.sh
@@ -5,11 +5,5 @@ conda activate roger
 cd nitrate
 nohup ./run_roger-sas-nitrate_no_irrigation.sh &
 nohup ./run_roger-sas-nitrate_no_irrigation_compaction.sh &
-# nohup ./run_roger-sas-nitrate_20-ufc.sh &
-# nohup ./run_roger-sas-nitrate_35-ufc.sh &
-# nohup ./run_roger-sas-nitrate_50-ufc.sh &
 nohup ./run_roger-sas-nitrate_crop-specific.sh &
-# nohup ./run_roger-sas-nitrate_20-ufc_compaction.sh &
-# nohup ./run_roger-sas-nitrate_35-ufc_compaction.sh &
-# nohup ./run_roger-sas-nitrate_50-ufc_compaction.sh &
 nohup ./run_roger-sas-nitrate_crop-specific_compaction.sh &
