3c5adcbd9a2d98461d8699f0bc063f27506f397b
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2025-06-20 12:18:06 +0200

    modified examples

--- examples/plot_scale/reckenholz/svat_crop_nitrate_monte_carlo/svat_crop_nitrate_adt_mc_lys8_slurm.sh
+++ examples/plot_scale/reckenholz/svat_crop_nitrate_monte_carlo/svat_crop_nitrate_adt_mc_lys8_slurm.sh
@@ -14,7 +14,6 @@ module load devel/miniforge
 module load lib/hdf5/1.12-gnu-14.2-openmpi-4.1
 export OMP_NUM_THREADS=1
 export OMPI_MCA_btl="^uct,ofi"
-export OMPI_MCA_pml="ucx"
 export OMPI_MCA_mtl="^ofi"
 eval "$(conda shell.bash hook)"
 conda activate roger-mpi
