6a88d15a717335150b01e36a437f98d7942a6731
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2025-08-01 15:42:59 +0200

    modified examples

---
+++ examples/plot_scale/bw_cropland/irrigation_soil-compaction/svat_crop_notzingen_winter-wheat_sugar-beet_silage-corn_crop-specific-irrigation_soil-compaction.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --time=4:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=1000
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-user=robin.schwemmle@hydrology.uni-freiburg.de
+#SBATCH --job-name=svat_crop_notzingen_winter-wheat_sugar-beet_silage-corn_crop-specific-irrigation_soil-compaction
+#SBATCH --output=svat_crop_notzingen_winter-wheat_sugar-beet_silage-corn_crop-specific-irrigation_soil-compaction.out
+#SBATCH --error=svat_crop_notzingen_winter-wheat_sugar-beet_silage-corn_crop-specific-irrigation_soil-compaction_err.out
+#SBATCH --export=ALL
+ 
+module load devel/miniforge
+eval "$(conda shell.bash hook)"
+conda activate roger
+cd /home/fr/fr_fr/fr_rs1092/roger/examples/plot_scale/bw_cropland//irrigation_soil-compaction
+ 
+python svat_crop.py -b numpy -d cpu --location notzingen --irrigation-scenario crop-specific --crop-rotation-scenario winter-wheat_sugar-beet_silage-corn -td "${TMPDIR}"
+# Move output from local SSD to global workspace
+echo "Move output to /pfs/10/work/fr_rs1092-workspace/bw_cropland/output/irrigation_soil-compaction/crop-specific"
+mkdir -p /pfs/10/work/fr_rs1092-workspace/bw_cropland/output/irrigation_soil-compaction/crop-specific
+mv "${TMPDIR}"/SVATCROP_*.nc /pfs/10/work/fr_rs1092-workspace/bw_cropland/output/irrigation_soil-compaction/crop-specific
