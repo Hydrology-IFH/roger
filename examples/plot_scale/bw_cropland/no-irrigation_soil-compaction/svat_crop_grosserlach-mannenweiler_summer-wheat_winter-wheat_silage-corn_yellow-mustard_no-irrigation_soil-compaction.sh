0265033ddf89a70e654e9ae1b7038fb0fbcba997
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2025-08-01 15:10:24 +0200

    modified examples

---
+++ examples/plot_scale/bw_cropland/no-irrigation_soil-compaction/svat_crop_grosserlach-mannenweiler_summer-wheat_winter-wheat_silage-corn_yellow-mustard_no-irrigation_soil-compaction.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --time=6:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=2000
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-user=robin.schwemmle@hydrology.uni-freiburg.de
+#SBATCH --job-name=svat_crop_grosserlach-mannenweiler_summer-wheat_winter-wheat_silage-corn_yellow-mustard_no-irrigation_soil-compaction
+#SBATCH --output=svat_crop_grosserlach-mannenweiler_summer-wheat_winter-wheat_silage-corn_yellow-mustard_no-irrigation_soil-compaction.out
+#SBATCH --error=svat_crop_grosserlach-mannenweiler_summer-wheat_winter-wheat_silage-corn_yellow-mustard_no-irrigation_soil-compaction_err.out
+#SBATCH --export=ALL
+ 
+eval "$(conda shell.bash hook)"
+conda activate roger
+cd /home/fr/fr_fr/fr_rs1092/roger/examples/plot_scale/bw_cropland//no-irrigation_soil-compaction
+ 
+python svat_crop.py -b numpy -d cpu --location grosserlach-mannenweiler --crop-rotation-scenario summer-wheat_winter-wheat_silage-corn_yellow-mustard -td "${TMPDIR}"
+# Move output from local SSD to global workspace
+echo "Move output to /pfs/10/work/fr_rs1092-workspace/bw_cropland/output/no-irrigation_soil-compaction"
+mkdir -p /pfs/10/work/fr_rs1092-workspace/bw_cropland/output/no-irrigation_soil-compaction
+mv "${TMPDIR}"/SVATCROP_*.nc /pfs/10/work/fr_rs1092-workspace/bw_cropland/output/no-irrigation_soil-compaction
