e73ebb879d25be32c7b2c9a1f340f85092404e8d
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2025-08-05 14:44:00 +0200

    modified examples

--- examples/plot_scale/bw_cropland/irrigation/write_job_scripts_slurm.py
+++ examples/plot_scale/bw_cropland/irrigation/write_job_scripts_slurm.py
@@ -29,11 +29,11 @@ def main():
                 output_path_ws = base_path_ws / "output" / dir_name / f"{irrigation_scenario}"
                 lines = []
                 lines.append("#!/bin/bash\n")
-                lines.append("#SBATCH --time=12:00:00\n")
+                lines.append("#SBATCH --time=4:00:00\n")
                 lines.append("#SBATCH --nodes=1\n")
                 lines.append("#SBATCH --ntasks=1\n")
                 lines.append("#SBATCH --cpus-per-task=1\n")
-                lines.append("#SBATCH --mem=2000\n")
+                lines.append("#SBATCH --mem=1000\n")
                 lines.append("#SBATCH --mail-type=FAIL\n")
                 lines.append("#SBATCH --mail-user=robin.schwemmle@hydrology.uni-freiburg.de\n")
                 lines.append(f"#SBATCH --job-name={script_name}\n")
--- examples/plot_scale/bw_cropland/no-irrigation_soil-compaction/svat_crop_mergentheim_summer-barley_no-irrigation_soil-compaction.sh
+++ examples/plot_scale/bw_cropland/no-irrigation_soil-compaction/svat_crop_mergentheim_summer-barley_no-irrigation_soil-compaction.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --time=12:00:00
+#SBATCH --time=4:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=2000
+#SBATCH --mem=1000
 #SBATCH --mail-type=FAIL
 #SBATCH --mail-user=robin.schwemmle@hydrology.uni-freiburg.de
 #SBATCH --job-name=svat_crop_mergentheim_summer-barley_no-irrigation_soil-compaction
