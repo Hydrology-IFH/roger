9cc1a203bd13d5f6e14ee9ebc824ec476d312339
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2025-08-06 16:44:32 +0200

    modified examples

--- examples/plot_scale/bw_cropland/irrigation/write_simulations_to_csv.sh
+++ examples/plot_scale/bw_cropland/irrigation/write_simulations_to_csv.sh
@@ -14,7 +14,7 @@
 module load devel/miniforge
 eval "$(conda shell.bash hook)"
 conda activate roger
-cd /pfs/10/work/fr_rs1092-workspace/roger/examples/plot_scale/bw_cropland//irrigation
+cd /pfs/10/work/fr_rs1092-workspace/roger/examples/plot_scale/bw_cropland/irrigation
  
 python write_simulations_of_lsv-stations_to_csv.py
 # python write_simulations_to_csv.py
