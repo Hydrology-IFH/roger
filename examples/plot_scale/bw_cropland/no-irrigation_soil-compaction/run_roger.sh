b0b35ddc3b689e38b71a87d64099f2cf957165ec
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2025-08-07 16:41:46 +0200

    modified examples

--- examples/plot_scale/bw_cropland/no-irrigation_soil-compaction/run_roger.sh
+++ examples/plot_scale/bw_cropland/no-irrigation_soil-compaction/run_roger.sh
@@ -25,8 +25,6 @@ python svat_crop.py -b numpy -d cpu --crop-rotation-scenario vegetables
 python svat_crop.py -b numpy -d cpu --crop-rotation-scenario strawberry
 python svat_crop.py -b numpy -d cpu --crop-rotation-scenario winter-wheat_clover
 python svat_crop.py -b numpy -d cpu --crop-rotation-scenario summer-wheat_winter-wheat
-python svat_crop.py -b numpy -d cpu --crop-rotation-scenario summer-wheat_clover_winter-wheat
-python svat_crop.py -b numpy -d cpu --crop-rotation-scenario winter-wheat_clover_silage-corn
 python svat_crop.py -b numpy -d cpu --crop-rotation-scenario winter-wheat_sugar-beet_silage-corn
 python svat_crop.py -b numpy -d cpu --crop-rotation-scenario summer-wheat_winter-wheat_silage-corn
 python svat_crop.py -b numpy -d cpu --crop-rotation-scenario summer-wheat_winter-wheat_winter-rape
