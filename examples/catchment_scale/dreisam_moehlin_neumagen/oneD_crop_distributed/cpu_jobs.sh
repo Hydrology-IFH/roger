eee8697c26d572643454e5c9e0232d889de51e0f
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2025-12-30 13:19:03 +0100

    modified job scripts

--- examples/catchment_scale/dreisam_moehlin_neumagen/oneD_crop_distributed/cpu_jobs.sh
+++ examples/catchment_scale/dreisam_moehlin_neumagen/oneD_crop_distributed/cpu_jobs.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+eval "$(conda shell.bash hook)"
 conda activate roger
 python oneD_crop.py -b jax -d cpu --stress-test-meteo base   
 # python oneD_crop.py -b jax -d cpu --stress-test-meteo spring-drought --stress-test-meteo-magnitude 2 --stress-test-meteo-duration 0   
--- examples/catchment_scale/dreisam_moehlin_neumagen/oneD_crop_distributed/gpu_jobs.sh
+++ examples/catchment_scale/dreisam_moehlin_neumagen/oneD_crop_distributed/gpu_jobs.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+eval "$(conda shell.bash hook)"
 conda activate roger-gpu
 python oneD_crop.py -b jax -d gpu --stress-test-meteo base   
 # python oneD_crop.py -b jax -d gpu --stress-test-meteo spring-drought --stress-test-meteo-magnitude 2 --stress-test-meteo-duration 0   
