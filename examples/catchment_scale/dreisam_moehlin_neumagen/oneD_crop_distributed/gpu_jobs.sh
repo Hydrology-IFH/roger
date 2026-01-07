20bc1676147a5d71153b8bbbfbab794fd45042c6
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2026-01-07 15:52:46 +0100

    modified gpu jobs

--- examples/catchment_scale/dreisam_moehlin_neumagen/oneD_crop_distributed/gpu_jobs.sh
+++ examples/catchment_scale/dreisam_moehlin_neumagen/oneD_crop_distributed/gpu_jobs.sh
@@ -2,6 +2,8 @@
 
 eval "$(conda shell.bash hook)"
 conda activate roger-gpu
-python oneD_crop.py -b jax -d gpu --stress-test-meteo base   
-# python oneD_crop.py -b jax -d gpu --stress-test-meteo spring-drought --stress-test-meteo-magnitude 2 --stress-test-meteo-duration 0   
+# python oneD_crop.py -b jax -d gpu --stress-test-meteo base   
+python oneD_crop.py -b jax -d gpu --stress-test-meteo spring-drought --stress-test-meteo-magnitude 2 --stress-test-meteo-duration 0   
 # python oneD_crop.py -b jax -d gpu --stress-test-meteo spring-drought --stress-test-meteo-magnitude 2 --stress-test-meteo-duration 0 --irrigation
+# python oneD_crop.py -b jax -d gpu --stress-test-meteo summer-drought --stress-test-meteo-magnitude 2 --stress-test-meteo-duration 0
+# python oneD_crop.py -b jax -d gpu --stress-test-meteo summer-drought --stress-test-meteo-magnitude 2 --stress-test-meteo-duration 0 --irrigation
