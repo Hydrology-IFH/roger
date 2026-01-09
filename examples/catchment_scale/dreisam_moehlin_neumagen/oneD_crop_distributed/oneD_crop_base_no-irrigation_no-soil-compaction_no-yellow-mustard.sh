e574d9b853a8a500e8c66877014d0cbfa8ca0b36
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2026-01-09 16:20:04 +0100

    added gpu job scripts

---
+++ examples/catchment_scale/dreisam_moehlin_neumagen/oneD_crop_distributed/oneD_crop_base_no-irrigation_no-soil-compaction_no-yellow-mustard.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#SBATCH --time=6:00:00
+#SBATCH --gres=gpu:a100:1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=32000
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-user=robin.schwemmle@hydrology.uni-freiburg.de
+#SBATCH --job-name=oneD_crop_base_no-irrigation_no-soil-compaction_no-yellow-mustard
+#SBATCH --output=oneD_crop_base_no-irrigation_no-soil-compaction_no-yellow-mustard.out
+#SBATCH --error=oneD_crop_base_no-irrigation_no-soil-compaction_no-yellow-mustard_err.out
+#SBATCH --export=ALL
+ 
+module load devel/miniforge
+conda activate roger-gpu
+cd /pfs/10/work/fr_rs1092-workspace/roger/examples/catchment_scale/dreisam_moehlin_neumagen/oneD_crop_distributed/oneD_crop_distributed
+ 
+mkdir ${TMPDIR}/roger
+mkdir ${TMPDIR}/roger/examples
+mkdir ${TMPDIR}/roger/examples/catchment_scale
+mkdir ${TMPDIR}/roger/examples/catchment_scale/dreisam_moehlin_neumagen
+mkdir ${TMPDIR}/roger/examples/catchment_scale/dreisam_moehlin_neumagen/output
+cp -r /pfs/10/work/fr_rs1092-workspace/roger/examples/catchment_scale/dreisam_moehlin_neumagen/oneD_crop_distributed/oneD_crop.py ${TMPDIR}/roger/examples/catchment_scale/dreisam_moehlin_neumagen/oneD_crop_distributed
+cp -r /pfs/10/work/fr_rs1092-workspace/roger/examples/catchment_scale/dreisam_moehlin_neumagen/oneD_crop_distributed/parameters_roger.nc ${TMPDIR}/roger/examples/catchment_scale/dreisam_moehlin_neumagen/oneD_crop_distributed
+cp -r /pfs/10/work/fr_rs1092-workspace/roger/examples/catchment_scale/dreisam_moehlin_neumagen/oneD_crop_distributed/config.yml ${TMPDIR}/roger/examples/catchment_scale/dreisam_moehlin_neumagen/oneD_crop_distributed
+cp -r /pfs/10/work/fr_rs1092-workspace/roger/examples/catchment_scale/dreisam_moehlin_neumagen/oneD_crop_distributed/input ${TMPDIR}/roger/examples/catchment_scale/dreisam_moehlin_neumagen/oneD_crop_distributed
+sleep 120
+cd ${TMPDIR}/roger/examples/catchment_scale/dreisam_moehlin_neumagen/oneD_crop_distributed
+python oneD_crop.py -b jax -d gpu --stress-test-meteo base    -td "${TMPDIR}"
+# Move output from local SSD to global workspace
+echo "Move output to /pfs/10/work/fr_rs1092-workspace/roger/examples/catchment_scale/dreisam_moehlin_neumagen/oneD_crop_distributed/output"
+mkdir -p /pfs/10/work/fr_rs1092-workspace/roger/examples/catchment_scale/dreisam_moehlin_neumagen/oneD_crop_distributed/output
+mv "${TMPDIR}"/output/ONEDCROP_*.nc /pfs/10/work/fr_rs1092-workspace/roger/examples/catchment_scale/dreisam_moehlin_neumagen/oneD_crop_distributed/output
