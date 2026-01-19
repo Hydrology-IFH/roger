b99aedea80b1558b1de5d88d38232712844f316a
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2026-01-19 14:56:26 +0100

    modified gpu job scripts

---
+++ examples/catchment_scale/dreisam_moehlin_neumagen/oneD_crop_distributed/write_roger_data_for_modflow.py
@@ -0,0 +1,89 @@
+import os
+import glob
+import h5netcdf
+import xarray as xr
+import datetime
+from pathlib import Path
+import numpy as onp
+import click
+import roger
+
+
+@click.command("main")
+def main():
+    base_path = Path(__file__).parent
+    # directory of results
+    base_path_output = Path("/Volumes/LaCie/roger/examples/catchment_scale/dreisam_moehlin_neumagen/oneD_crop_distributed") / "output"
+    if not os.path.exists(base_path_output):
+        os.mkdir(base_path_output)
+
+    irrigation_scenarios = ["no-irrigation"]
+    catch_crop_scenarios = ["no-yellow-mustard"]
+    soil_compaction_scenarios = ["no-soil-compaction"]
+    meteo_stress = ["base"]
+    magnitudes = [2]
+    durations = [0]
+
+    meteo_stress_tests = []
+    for meteo in meteo_stress:
+        if meteo == "base" or meteo == "base_2000-2024":
+            meteo_stress_tests.append(meteo_stress_tests.append(f"{meteo}-magnitude0-duration0"))
+        else:
+            for magnitude in magnitudes:
+                for duration in durations:
+                    meteo_stress_tests.append(f"{meteo}-magnitude{magnitude}-duration{duration}")
+
+    for meteo_stress_test in meteo_stress_tests:
+        for irrigation_scenario in irrigation_scenarios:
+            for catch_crop_scenario in catch_crop_scenarios:
+                for soil_compaction_scenario in soil_compaction_scenarios:
+                    # merge model output into single file
+                    diag_file = str(base_path_output / f"ONEDCROP_{meteo_stress_test}_{irrigation_scenario}_{catch_crop_scenario}_{soil_compaction_scenario}.rate.nc")
+                    if os.path.exists(diag_file):
+                        output_file = base_path_output / f"recharge_{meteo_stress_test}_{irrigation_scenario}_{catch_crop_scenario}_{soil_compaction_scenario}.nc"
+                        if not os.path.exists(output_file):
+                            with h5netcdf.File(output_file, "w", decode_vlen_strings=False) as f:
+                                f.attrs.update(
+                                    date_created=datetime.datetime.today().isoformat(),
+                                    title="RoGeR recharge simulations for the Dreisam-Moehlin-Neumagen catchment",
+                                    institution="University of Freiburg, Chair of Hydrology",
+                                    references="",
+                                    comment="First timestep (t=0) contains initial values. Simulations start are written from second timestep (t=1) to last timestep (t=N).",
+                                    model_structure="ONED model with free drainage and explicit crop growth dynamics",
+                                    roger_version=f"{roger.__version__}",
+                                )
+                                # collect dimensions
+                                with h5netcdf.File(diag_file, "r", decode_vlen_strings=False) as df:
+                                    # set dimensions with a dictionary
+                                    dict_dim = {
+                                        "x": len(df.variables["x"]),
+                                        "y": len(df.variables["y"]),
+                                        "Time": len(df.variables["Time"]),
+                                    }
+                                    time = onp.array(df.variables.get("Time"))
+
+                                    if not f.dimensions:
+                                        f.dimensions = dict_dim
+                                        v = f.create_variable("x", ("x",), float, compression="gzip", compression_opts=1)
+                                        v.attrs["long_name"] = "x"
+                                        v.attrs["units"] = "m"
+                                        v[:] = onp.arange(dict_dim["x"]) * 25
+                                        v = f.create_variable("y", ("y",), float, compression="gzip", compression_opts=1)
+                                        v.attrs["long_name"] = "y"
+                                        v.attrs["units"] = "m"
+                                        v[:] = onp.arange(dict_dim["y"]) * 25
+                                        v = f.create_variable("Time", ("Time",), float, compression="gzip", compression_opts=1)
+                                        var_obj = df.variables.get("Time")
+                                        v.attrs.update(time_origin=var_obj.attrs["time_origin"], units=var_obj.attrs["units"])
+                                        v[:] = time
+                                for key in ["q_ss"]:
+                                    with h5netcdf.File(diag_file, "r", decode_vlen_strings=False) as df:
+                                        var_obj = df.variables.get(key)
+                                        v = f.create_variable(key, ("Time", "y", "x"), float, compression="gzip", compression_opts=1)
+                                        v[:, :, :] = var_obj
+                                        v.attrs.update(long_name=var_obj.attrs["long_name"], units=var_obj.attrs["units"])
+    return
+
+
+if __name__ == "__main__":
+    main()
