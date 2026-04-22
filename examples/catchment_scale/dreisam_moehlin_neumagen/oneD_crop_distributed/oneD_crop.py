19d82c540b854912b390da64cb97e199bc638265
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2026-04-22 15:58:17 +0200

    fixed bug in potential transpiration

--- examples/catchment_scale/dreisam_moehlin_neumagen/oneD_crop_distributed/oneD_crop.py
+++ examples/catchment_scale/dreisam_moehlin_neumagen/oneD_crop_distributed/oneD_crop.py
@@ -150,8 +150,7 @@ def main(stress_test_meteo, stress_test_meteo_magnitude, stress_test_meteo_durat
             # total grid numbers in x- and y-direction
             settings.nx, settings.ny = self._config["nx"], self._config["ny"]
             # derive total number of time steps from forcing
-            # settings.runlen = self._get_runlen(self._input_dir, "forcing.nc")
-            settings.runlen = 280 * 24 * 60 * 60
+            settings.runlen = self._get_runlen(self._input_dir, "forcing.nc")
             settings.nitt_forc = len(self._read_var_from_nc("Time", self._input_dir, 'forcing.nc'))
             station_ids = onp.unique(self._read_var_from_nc_xr("station_id", self._base_path, "parameters_roger.nc"))
             station_ids = station_ids[~onp.isnan(station_ids)]
